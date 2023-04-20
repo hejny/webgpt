34d98754ba50f01f65e4bd42d0a600f35d1f19b3
Pavol Hejny <me@pavolhejny.com>
2023-04-21 00:21:03 +0200

    Cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import { WelcomeSection } from '../sections/10-Welcome/Welcome';
 import { PavolHejnySection } from '../sections/20-PavolHejny/PavolHejny';
 import { ServicesSection } from '../sections/30-Services/Services';
 import { ReferencesSection } from '../sections/40-References/References';
-import { MyProjectsSection } from '../sections/50-MyProjects/MyProjects';
 import { ContactSection } from '../sections/70-Contact/Contact';
 import { FooterSection } from '../sections/90-Footer/Footer';
 import styles from '../styles/common.module.css';
@@ -40,15 +39,9 @@ export default function IndexPage({ lang }: any) {
                     {/* TODO: <PresentationSection /> */}
                     {/* TODO: <PrototypingSection /> */}
                     {/* TODO: <TechnologiesSection /> */}
-                    {/* TODO: <PricingSection />
-                                Cena za služby
-                                Vůbec nejsem fanoušek ceny za hodinu – ve světě rychle automatizace jde pouze o o velmi o krok naprosto špatným směrem který penalizuje efektivitu
-                                Přijímám platby v kryptoměnách nejen v Bitcoinu ale v jste, kterou chcete
-                    */}
 
                     {/* <WhereToMeetMe> */}
                     <ReferencesSection variant="SHORT" />
-                    <MyProjectsSection />
                     {/* TODO: Finish or remove <HackingSection /> */}
                     <ContactSection variant="SHORT" />
                     {/* <SampleSection variant="SHORT" /> */}
