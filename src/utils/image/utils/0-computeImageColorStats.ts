fac81e502cd778dc273f1f613767c8826ce2377c
Pavol Hejny <me@pavolhejny.com>
2023-05-03 18:49:48 +0200

    First run of new color stats

--- src/utils/image/utils/0-computeImageColorStats.ts
+++ src/utils/image/utils/0-computeImageColorStats.ts
@@ -1,24 +1,19 @@
-import { Vector } from 'xyzt';
 import { Image } from '../Image';
 import { computeImageAverageColor } from './computeImageAverageColor';
 import { computeImageDarkestColor } from './computeImageDarkestColor';
 import { computeImageLightestColor } from './computeImageLightestColor';
-import { computeImageMostFrequentColor } from './computeImageMostFrequentColor';
-import { computeImageMostGroupedColor } from './computeImageMostGroupedColor';
-import { computeImageMostIsolatedColor } from './computeImageMostIsolatedColor';
-import { computeImageMostSaturatedColor } from './computeImageMostSaturatedColor';
 import { IImageColorStats, IImageColorStatsRegion } from './IImageColorStats';
 
 /**
  * Compute the image color statistics
  */
 export function computeImageColorStats(image: Image): IImageColorStats {
-    const bottomImage = image.crop(new Vector(0, image.height * 0.62), new Vector(image.width, image.height));
+    // !!! const bottomImage = image.crop(new Vector(0, image.height * 0.62!!!), new Vector(image.width, image.height));
 
     return {
         ...computeWholeImageColorStats(image),
-        bottom: computeWholeImageColorStats(bottomImage),
-    };
+        // !!!! bottom: computeWholeImageColorStats(bottomImage),
+    } as any /* <- !!! */;
 }
 
 /**
@@ -29,12 +24,14 @@ function computeWholeImageColorStats(image: Image): IImageColorStatsRegion {
         averageColor: computeImageAverageColor(image),
         lightestColor: computeImageLightestColor(image),
         darkestColor: computeImageDarkestColor(image),
+        /*  !!!!
         mostFrequentColor: computeImageMostFrequentColor(image),
         leastFrequentColor: computeImageMostFrequentColor(image),
         mostSaturatedColor: computeImageMostSaturatedColor(image),
         mostIsolatedColor: computeImageMostIsolatedColor(image),
         mostGroupedColor: computeImageMostGroupedColor(image),
-    };
+        */
+    } as any /* <- !!! */;
 }
 
 /**
