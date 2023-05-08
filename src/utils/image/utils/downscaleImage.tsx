1be6de0761abae5352b17063b973cbc4d9560269
Pavol Hejny <me@pavolhejny.com>
2023-05-08 13:31:04 +0200

    Compute color stats from downscaled image by size

---
+++ src/utils/image/utils/downscaleImage.tsx
@@ -0,0 +1,30 @@
+
+import { IImage } from '../IImage';
+import { Image } from '../Image';
+
+/**
+ * @@@
+ */
+export function scaleImage(image: IImage, ratio: number): Image {
+    const newSize = image.size.scale(ratio);
+    const newImage = new Image(newSize);
+
+
+
+    for (let y = 0; y < newSize.y; y++) {
+    
+        for (let x = 0; x < newSize.x; x++) {
+            const oldX = Math.floor((x * image.width) / newImage.width);
+            const oldY = Math.floor((y * image.height) / newImage.height);
+        
+            newImage.setPixel({x,y},image.getPixel({x:oldX,y:oldY})));
+        }
+    }
+
+   return newImage;
+}
+
+
+/**
+ * TODO: Do some smart scaling then nearest neighbor
+ */
