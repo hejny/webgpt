68ad99e14bcb9e22876bc2134c0e6802c20fe1f7
Pavol Hejny <me@pavolhejny.com>
2023-05-18 09:57:59 +0200

    Random sort fonts in prompt

--- scripts/generate-wallpapers-content/generate-wallpapers-content.ts
+++ scripts/generate-wallpapers-content/generate-wallpapers-content.ts
@@ -191,7 +191,12 @@ function createFontPromptTemplate() {
                 Write me a Google font which is best fitting for the website.
 
                 Pick from the list:
-                ${block(FONTS.map((fontName) => `- ${fontName}`).join('\n'))}
+                ${block(
+                    [...FONTS]
+                        .sort(() => Math.random() - 0.5)
+                        .map((fontName) => `- ${fontName}`)
+                        .join('\n'),
+                )}
 
 
                 Write just the font name nothing else.
