9465ef549a9c4f72e7fb3790f9d9790ddccdd9be
Pavol Hejny <me@pavolhejny.com>
2023-05-18 09:38:39 +0200

    Enhance prompt for texts

--- scripts/generate-wallpapers-content/generate-wallpapers-content.ts
+++ scripts/generate-wallpapers-content/generate-wallpapers-content.ts
@@ -155,19 +155,6 @@ async function generateWallpapersContent({ isCommited, parallel }: { isCommited:
 function createContentPromptTemplate() {
     return randomItem(
         `
-            Write me markdown content of website with wallpaper:
-
-            "🟦"
-
-            The header of the page should not be copy of the text but rather a real content of the website which is using this wallpaper.
-
-            - Feel free to use structure like headings, bullets, numbering, blockquotes, paragraphs, horizontal lines, etc.
-            - You can use formatting like bold or _italic_
-            - You can include UTF-8 emojis
-            - Links should be only #hash anchors (and you can refer to the document itself)
-            - Do not include images
-        `,
-        `
             Write me content for website with wallpaper which alt text is:
 
             "🟦"
@@ -176,7 +163,9 @@ function createContentPromptTemplate() {
 
             - Use markdown format 
             - Start with the heading
+            - Heading should be short and concise
             - The content should look like a real website 
+            - The website should not be about the wallpaper, wallpaper is just a related background
             - Include real sections like references, contact, user stories, etc. use things relevant to the page purpose.
             - Feel free to use structure like headings, bullets, numbering, blockquotes, paragraphs, horizontal lines, etc.
             - You can use formatting like bold or _italic_
@@ -190,9 +179,6 @@ function createContentPromptTemplate() {
             "🟦"
 
             The header of the page should not be copy of the text but rather a real content of the website which is using this wallpaper.
-        `,
-        `
-            Write me content for website with wallpaper "🟦"
         `,
     );
 }
