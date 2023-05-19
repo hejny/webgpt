51a24f7b698b55a191f07213f7ad5a2dc2c580b6
Pavol Hejny <me@pavolhejny.com>
2023-05-19 12:48:25 +0200

    Scale image by linear interpolation

--- src/utils/image/utils/scaleImage.ts
+++ src/utils/image/utils/scaleImage.ts
@@ -6,17 +6,25 @@ import { Image } from '../Image';
  * @@@
  */
 export function scaleImage(image: IImage, ratio: number): Image {
-    const newSize = image.size.scale(ratio).map(Math.ceil);
+    const newSize = image.size.scale(ratio);
     const newImage = new Image(newSize);
 
     for (let y = 0; y < newSize.y; y++) {
         for (let x = 0; x < newSize.x; x++) {
-            const startX = Math.floor(x / ratio);
-            const startY = Math.floor(y / ratio);
-            const endX = Math.ceil((x + 1) / ratio) - 1;
-            const endY = Math.ceil((y + 1) / ratio) - 1;
+            const oldX = (x * image.width) / newImage.width;
+            const oldY = (y * image.height) / newImage.height;
 
-            const pixel = averagePixels(image, startX, startY, endX, endY);
+            const x1 = Math.floor(oldX);
+            const y1 = Math.floor(oldY);
+            const x2 = Math.ceil(oldX);
+            const y2 = Math.ceil(oldY);
+
+            const q11 = image.getPixel({ x: x1, y: y1 });
+            const q12 = image.getPixel({ x: x1, y: y2 });
+            const q21 = image.getPixel({ x: x2, y: y1 });
+            const q22 = image.getPixel({ x: x2, y: y2 });
+
+            const pixel = interpolate(q11, q12, q21, q22, oldX - x1, oldY - y1);
             newImage.setPixel({ x, y }, pixel);
         }
     }
@@ -27,32 +35,24 @@ export function scaleImage(image: IImage, ratio: number): Image {
 /**
  * @@@
  */
-function averagePixels(image: IImage, startX: number, startY: number, endX: number, endY: number): Color {
-    let sumR = 0;
-    let sumG = 0;
-    let sumB = 0;
-    // [🚇] let sumA = 0;
-    const count = (endX - startX) * (endY - startY);
+function interpolate(q11: Color, q12: Color, q21: Color, q22: Color, x: number, y: number): Color {
+    const interpolateChannel = (c1: number, c2: number, c3: number, c4: number, x: number, y: number): number => {
+        const t = x * (1 - y);
+        const u = x * y;
+        return Math.round(c1 * (1 - t) * (1 - u) + c2 * t * (1 - u) + c3 * (1 - t) * u + c4 * t * u);
+    };
 
-    for (let y = startY; y < endY; y++) {
-        for (let x = startX; x < endX; x++) {
-            const pixel = image.getPixel({ x, y });
-            sumR += pixel.r;
-            sumG += pixel.g;
-            sumB += pixel.b;
-            // [🚇] sumA += pixel.a;
-        }
-    }
+    const r = interpolateChannel(q11.r, q12.r, q21.r, q22.r, x, y);
+    const g = interpolateChannel(q11.g, q12.g, q21.g, q22.g, x, y);
+    const b = interpolateChannel(q11.b, q12.b, q21.b, q22.b, x, y);
+    const a = interpolateChannel(q11.a, q12.a, q21.a, q22.a, x, y);
 
-    const avgR = Math.round(sumR / count);
-    const avgG = Math.round(sumG / count);
-    const avgB = Math.round(sumB / count);
-    // [🚇] const avgA = Math.round(sumA / count);
-
-    return Color.fromValues(avgR, avgG, avgB /* [🚇] , avgA*/);
+    return Color.fromValues(r, g, b, a);
 }
 
 /**
+ * TODO: Maybe implement via avarage https://sharegpt.com/c/me0ZGD2
  * TODO: Work internally with IVector and mix color operator
  * Note: [🚇] We do not need alpha channel for this application
+ * TODO: !!!! ACRY apply [🚇]
  */
