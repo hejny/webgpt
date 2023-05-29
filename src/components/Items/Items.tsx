0df4c7379adcd52d3e0d2d0b57ab1fe3f22bbdb9
Pavol Hejny <me@pavolhejny.com>
2023-05-29 14:56:00 +0200

    Remove itemsOnRow count itemsOnRow dynamically according to page width

--- src/components/Items/Items.tsx
+++ src/components/Items/Items.tsx
@@ -1,15 +1,14 @@
 import { ReactNode } from 'react';
-import { classNames } from '../../utils/classNames';
 import styles from './Items.module.css';
 
 /**
  * A functional component that renders a grid of items ⁘
- * 
+ *
  * @param {ItemsProps} props - The props for the component
  * @returns {JSX.Element} The JSX element for the component
  */
 interface ItemsProps {
-    itemsOnRow?: 2 | 3;
+    // TODO: preferedItemWidth: number;
     children: ReactNode;
 }
 
@@ -17,6 +16,6 @@ interface ItemsProps {
  * @@@
  */
 export function Items(props: ItemsProps) {
-    const { itemsOnRow = 3, children } = props;
-    return <div className={classNames(styles.items, styles[`row-with-${itemsOnRow}-items`])}>{children}</div>;
+    const { children } = props;
+    return <div className={styles.items}>{children}</div>;
 }
