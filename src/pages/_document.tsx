59c0ea733d21cf3a317c6f5c930597cb1a8b8a55
Pavol Hejny <me@pavolhejny.com>
2023-05-31 20:48:16 +0200

    AnalyticsAndIntegrations

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { AnalyticsAndIntegrations } from '../components/AnalyticsAndIntegrations/AnalyticsAndIntegrations';
 
 export default function Document() {
     return (
         <Html>
             <Head />
+            <AnalyticsAndIntegrations />
             <body>
                 <Main />
 
