39e8aff23b1baa5dffa3b475560dc55762d0a000
Pavol Hejny <me@pavolhejny.com>
2023-06-01 16:54:22 +0200

    Aigen designs

--- src/components/Aigen/Aigen.tsx
+++ src/components/Aigen/Aigen.tsx
@@ -1,91 +1,120 @@
+import React from 'react';
+import { Color } from '../../utils/color/Color';
+import { textColor } from '../../utils/color/operators/furthest';
+import { colorLuminance } from '../../utils/color/utils/colorLuminance';
+import { useSsrDetection } from '../../utils/hooks/useSsrDetection';
+import { useWallpaper } from '../../utils/hooks/useWallpaper';
+import { randomItem } from '../../utils/randomItem';
+
 /**
  * @@
  */
 export function Aigen() {
+    const isRunningOnServer = useSsrDetection();
+    const {
+        colorStats: { palette },
+    } = useWallpaper();
+
+    if (isRunningOnServer) {
+        // TODO: !! Allow when decided how to mark AI generated webpages
+        return <></>;
+    }
+
+    let colors = palette.map(({ value }) => value).sort((a, b) => colorLuminance(b) - colorLuminance(a));
+
+    if (colors.length < 2) {
+        colors = [
+            Color.fromHex('#ffcf86'),
+            Color.fromHex('#ff723d'),
+            Color.fromHex('#912920'),
+            Color.fromHex('#792e33'),
+            Color.fromHex('#270f1c'),
+        ];
+    } else if (colors.length < 9) {
+        colors = [...colors, ...colors, ...colors, ...colors, ...colors, ...colors];
+    }
+
+    const letters = randomItem(
+        [
+            { letter: 'A', color: colors[0] },
+            { letter: 'I', color: colors[1] },
+            { letter: 'G', color: colors[3] },
+            { letter: 'E', color: colors[4] },
+            { letter: 'N', color: colors[5] },
+        ],
+        [
+            { letter: 'A', color: colors[0] },
+            { letter: 'I', color: colors[1] },
+        ],
+        [
+            { letter: 'G', color: colors[0] },
+            { letter: 'E', color: colors[1] },
+            { letter: 'N', color: colors[3] },
+            { letter: 'A', color: colors[4] },
+            { letter: 'I', color: colors[5] },
+        ],
+        [
+            { letter: 'G', color: colors[0] },
+            { letter: 'E', color: colors[1] },
+            { letter: 'N', color: colors[3] },
+            { letter: 'E', color: colors[4] },
+            { letter: 'R', color: colors[5] },
+            { letter: 'A', color: colors[6] },
+            { letter: 'T', color: colors[7] },
+            { letter: 'E', color: colors[8] },
+            { letter: 'D', color: colors[9] },
+        ],
+    );
+
+    const y = randomItem('50%', '60%', '65%');
+    const fontSize = randomItem('13', '14', '15', '16');
+    const fontWeight = randomItem('normal', 'bold');
+    const fontFamily = randomItem('monospace', 'sans-serif', 'serif', /*'cursive',*/ 'fantasy');
+
+    const height = 10 + 30 * Math.random();
+    const width = height * letters.length;
+
     return (
         <div
             style={{
                 zIndex: 99999999,
-                position: 'absolute' /* <- TODO: [🧠] absolute vs fixed*/,
+                position: randomItem('absolute', 'fixed') /* <- TODO: [🧠] absolute vs fixed*/,
                 top: 0,
                 right: 0,
             }}
         >
             <a href="#" /* <- TODO: !!! Create web which will explain how is the web AI generated */>
-                <svg xmlns="http://www.w3.org/2000/svg" width="75" height="15" view-box="0 0 100 20">
-                    <rect x="0" y="0" width="20%" height="100%" fill="#ffcf86" />
-                    <text
-                        x="10%"
-                        y="65%"
-                        dominant-baseline="middle"
-                        text-anchor="middle"
-                        fill="#000"
-                        font-size="14"
-                        font-weight="bold"
-                    >
-                        A
-                    </text>
-
-                    <rect x="20%" y="0" width="20%" height="100%" fill="#ff723d" />
-                    <text
-                        x="30%"
-                        y="65%"
-                        dominant-baseline="middle"
-                        text-anchor="middle"
-                        fill="#000"
-                        font-size="14"
-                        font-weight="bold"
-                    >
-                        I
-                    </text>
-
-                    <rect x="40%" y="0" width="20%" height="100%" fill="#912920" />
-                    <text
-                        x="50%"
-                        y="65%"
-                        dominant-baseline="middle"
-                        text-anchor="middle"
-                        fill="#fff"
-                        font-size="14"
-                        font-weight="bold"
-                    >
-                        G
-                    </text>
-
-                    <rect x="60%" y="0" width="20%" height="100%" fill="#792e33" />
-                    <text
-                        x="70%"
-                        y="65%"
-                        dominant-baseline="middle"
-                        text-anchor="middle"
-                        fill="#fff"
-                        font-size="14"
-                        font-weight="bold"
-                    >
-                        E
-                    </text>
-
-                    <rect x="80%" y="0" width="20%" height="100%" fill="#270f1c" />
-                    <text
-                        x="90%"
-                        y="65%"
-                        dominant-baseline="middle"
-                        text-anchor="middle"
-                        fill="#fff"
-                        font-size="14"
-                        font-weight="bold"
-                    >
-                        N
-                    </text>
-                </svg>
+                {randomItem(
+                    <div style={{ marginRight: 5 }}>← Generated by AI</div>,
+                    <svg xmlns="http://www.w3.org/2000/svg" {...{ width, height }}>
+                        {letters.map(({ letter, color }, i) => (
+                            <React.Fragment key={i}>
+                                <rect
+                                    x={`${i * (100 / letters.length)}%`}
+                                    y="0"
+                                    width={`${100 / letters.length}%`}
+                                    height="100%"
+                                    fill={color.toHex()}
+                                />
+                                <text
+                                    x={`${(i + 0.5) * (100 / letters.length)}%`}
+                                    dominant-baseline="middle"
+                                    text-anchor="middle"
+                                    fill={color.then(textColor).toHex()}
+                                    {...{ y, fontSize, fontWeight, fontFamily }}
+                                >
+                                    {letter}
+                                </text>
+                            </React.Fragment>
+                        ))}
+                    </svg>,
+                )}
             </a>
         </div>
     );
 }
 
 /**
- * TODO: !!! Use real Palette
- * TODO: !!! font-family
  * TODO: [🧠] Test / think about / ask how to properly mark AI generated webpages
  * TODO: Maybe as external image
  * TODO: alt="← Generated by AI"
