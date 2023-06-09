817652beb175b3d73ca6c362b0632771d3fd80b4
Pavol Hejny <me@pavolhejny.com>
2023-06-09 19:30:38 +0200

    Wallpapers min

---
+++ src/pages/api/wallpapers-min.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getWallpapers } from '../../../scripts/utils/wallpaper/getWallpapers';
+import { string_color, string_wallpaper_id } from '../../utils/typeAliases';
+
+interface WallpapersResponse {
+    wallpapers: Array<{ id: string_wallpaper_id; primaryColor: string_color }>;
+}
+export default async function wallpapersIdsHandler(
+    request: NextApiRequest,
+    response: NextApiResponse<WallpapersResponse>,
+) {
+    response.status(200).json({
+        wallpapers: (await getWallpapers()).map(({ id, colorStats }) => ({
+            id,
+            primaryColor: colorStats.palette[0].value as any as string,
+        })),
+    });
+}
