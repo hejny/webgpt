c950dbcb3623aaeb6ac5a335dc13173efbb03cba
Pavol Hejny <me@pavolhejny.com>
2023-06-23 14:20:46 +0200

    WhoisDomain.css

--- src/components/WhoisDomains/WhoisDomain/WhoisDomain.tsx
+++ src/components/WhoisDomains/WhoisDomain/WhoisDomain.tsx
@@ -25,17 +25,17 @@ export function WhoisDomain(props: WhoisDomainProps) {
     const domainStatus = !whois ? 'PENDING' : getDomainStatusFromWhois(whois);
 
     return (
-        <div className={styles.WhoisDomain}>
+        <>
             {
                 {
-                    PENDING: <span style={{ color: '#888888' }}>Getting whois information</span>,
-                    AVAILABLE: <span style={{ color: '#0bee57' }}>Domain is available for registration</span>,
-                    REGISTERED: <span style={{ color: '#dd1b1b' }}>Domain is already registered</span>,
-                    UNKNOWN: <span style={{ color: '#888888' }}>Domain status is unknown</span>,
+                    PENDING: <span className={styles.pending}>Getting whois about {domain}</span>,
+                    AVAILABLE: <span className={styles.available}>Domain {domain} is available for registration</span>,
+                    REGISTERED: <span className={styles.registered}>Domain {domain} is already registered</span>,
+                    UNKNOWN: <span className={styles.unknown}>Domain status of {domain} is unknown</span>,
                 }[domainStatus]
             }
 
             {/**/ <pre>{JSON.stringify(whois, null, 4)}</pre> /**/}
-        </div>
+        </>
     );
 }
