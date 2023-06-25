49b32e219c60d5e64e254ac1c6f834c469210a66
Pavol Hejny <me@pavolhejny.com>
2023-06-25 15:29:09 +0200

    Better register-script

--- src/pages/api/register-script.ts
+++ src/pages/api/register-script.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { spaceTrim } from 'spacetrim';
 import { NEXT_PUBLIC_URL } from '../../../config';
+import { prettifyJavascript } from '../../export/utils/prettifyJavascript';
 
 async function register() {
     console.info('🔌', 'Registering your page');
@@ -8,36 +9,47 @@ async function register() {
     console.info('🔌', 'hostname', window.location.hostname);
     console.info('🔌', 'host', window.location.host);
 
-    const response = await fetch(`${(window as any).NEXT_PUBLIC_URL.href}/api/register`, {
-        method: 'POST',
-        body: JSON.stringify({ host: window.location.host }),
-    });
+    const response = await fetch(
+        `${
+            // @ts-ignore
+            config.NEXT_PUBLIC_URL.href
+        }/api/register`,
+        {
+            method: 'POST',
+            body: JSON.stringify({ host: window.location.host }),
+        },
+    );
     const { message } = (await response.json()) as any;
 
     console.info('🔌', { message });
 }
 
 export default async function registerScriptHandler(request: NextApiRequest, response: NextApiResponse) {
-    // !!! Remove !!! const registerScript = await readFile(join(__dirname, 'register-script.js.txt') /* <- TODO: Cache */, 'utf8');
-
     return response
         .status(200)
         .setHeader('content-type', 'text/javascript')
         .end(
-            spaceTrim(
-                // !!! Add note + remove register-script.js.txt
-                (block) => `
+            prettifyJavascript(
+                spaceTrim(
+                    // !!! Add note
+                    (block) => `
 
-                    /**
-                     * Note: [🔌]
-                     **/
-            
-                    const NEXT_PUBLIC_URL = new URL('${NEXT_PUBLIC_URL.href}');
+                        /**
+                         * Note: [🔌]
+                         **/
+                
+                        (()=>{
+                            
+                            const config = { NEXT_PUBLIC_URL: '${NEXT_PUBLIC_URL.href}' };
 
-                    /* not await */ register();
-        
-                    ${block(register.toString())}
-                `,
+                            /* not await */ register();
+                
+                            ${block(register.toString())}
+
+                        })();
+                    
+                    `,
+                ),
             ),
         );
 }
