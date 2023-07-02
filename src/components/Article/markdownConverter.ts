31d6d31b8325c6d4706b67eedb10eac0d84f91e0
Pavol Hejny <me@pavolhejny.com>
2023-07-02 13:34:52 +0200

    Add markdownConverter to the project
    -	Add tests for markdownConverter.makeHtml and markdownConverter.makeMarkdown
    -	Implemented markdownConverter.makeHtml and markdownConverter.makeMarkdown functions
    -	Add showdown and showdown-highlight as dependencies for markdownConverter
    -	Configure markdownConverter with flavor 'github' and extension showdownHighlight
    -	Code comment to lazy-make converter

---
+++ src/components/Article/markdownConverter.ts
@@ -0,0 +1,23 @@
+import { Converter } from 'showdown';
+import showdownHighlight from 'showdown-highlight';
+
+/**
+ * A converter instance that uses showdown and highlight extensions ⁘
+ *
+ * @type {Converter}
+ */
+export const markdownConverter = new Converter({
+    flavor: 'github',
+    extensions: [
+        showdownHighlight({
+            // Whether to add the classes to the <pre> tag, default is false
+            pre: true,
+            // Whether to use hljs' auto language detection, default is true
+            auto_detection: true,
+        }),
+    ],
+});
+
+/**
+ * TODO: !!! Lazy-make converter
+ */
