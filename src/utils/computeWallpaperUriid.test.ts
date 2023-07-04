2aac45c1f046c9e8d80612bc2921ddfc2b546329
Pavol Hejny <me@pavolhejny.com>
2023-07-04 23:25:15 +0200

    Fix test

--- src/utils/computeWallpaperUriid.test.ts
+++ src/utils/computeWallpaperUriid.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from '@jest/globals';
+import spaceTrim from 'spacetrim';
 import { computeWallpaperUriid } from './computeWallpaperUriid';
 import { hydrateColorStats } from './image/utils/hydrateColorStats';
+import { validateUuid } from './validateUuid';
 
 describe(`computeWallpaperUriid`, () => {
     it(`is compute deterministic uriid`, () => {
@@ -9,6 +11,7 @@ describe(`computeWallpaperUriid`, () => {
                 parent: 'ocean-vibes-1kmp5dwt35su',
                 src: 'https://cdn.midjourney.com/6be2b125-4fbb-498f-8f08-fc153998fef5/0_3.png',
                 prompt: 'A beautiful sunset over the ocean',
+                author: validateUuid('8450ee88-d216-41c4-a30e-5bba49289573'),
                 colorStats: hydrateColorStats({
                     palette: [
                         {
@@ -232,299 +235,67 @@ describe(`computeWallpaperUriid`, () => {
                         { count: 1, value: '#558888' },
                     ],
                 }),
-                content:
-                    '<p></p><div style="font-family: Poppins, sans-serif;"><p></p>\n<h1 id="oceanvibes">Ocean Vibes</h1>\n<p>Welcome to Ocean Vibes, a website dedicated to exploring the wonders of the ocean. Our stunning wallpaper featuring a beautiful sunset over the ocean sets the tone for our passion for all things related to the sea.</p>\n<h2 id="aboutus">About Us</h2>\n<p>At Ocean Vibes, we are committed to sharing our love and knowledge of the ocean with others. We offer a variety of resources, including articles on marine life, conservation efforts, travel destinations, and much more.</p>\n<h2 id="userstories">User Stories</h2>\n<p>Our community is made up of people from all walks of life who share a common love for the ocean. Here are some stories from our users:</p>\n<ul>\n<li>"I\'ve always been fascinated by the ocean, but never knew where to start. Ocean Vibes has given me a wealth of information and inspiration to explore this amazing world." - Emily, 28</li>\n<li>"The articles on Ocean Vibes have helped me plan my dream trip to Hawaii. I can\'t wait to explore the coral reefs and see all the incredible marine life." - Mark, 35</li>\n<li>"As an avid scuba diver, I appreciate the dedication Ocean Vibes has to promoting responsible tourism and conservation efforts. It\'s important to protect the ocean so future generations can enjoy it too." - Sarah, 42</li>\n</ul>\n<h2 id="resources">Resources</h2>\n<p>Check out some of our most popular articles and resources:</p>\n<ul>\n<li><a href="#best-beaches">10 Best Beaches in the World</a></li>\n<li><a href="#responsible-tourism">Protecting Our Oceans: Tips for Responsible Tourism</a></li>\n<li><a href="#coral-reefs">The Fas<wbr>ci<wbr>na<wbr>ting World of Coral Reefs</a></li>\n</ul>\n<h2 id="contactus">Contact Us</h2>\n<p>Do you have questions or suggestions? We\'d love to hear from you! Reach out to us at <a href="mailto:oceanvibes@example.com">oceanvibes@example.com</a>.</p>\n<p>Thank you for visiting Ocean Vibes. Let\'s work together to preserve the beauty of the ocean for generations to come 🐬🌊.</p>\n<hr>\n<h3 id="bestbeaches">Best Beaches</h3>\n<ol>\n<li>Anse Source d\'Argent, Seychelles</li>\n<li>Whitehaven Beach, Australia</li>\n<li>Navagio Beach, Greece</li>\n<li>Pink Sands Beach, Bahamas</li>\n<li>Maya Bay, Thailand</li>\n<li>Tulum Beach, Mexico</li>\n<li>Matira Beach, French Polynesia</li>\n<li>Seven Mile Beach, Jamaica</li>\n<li>Horseshoe Bay, Bermuda</li>\n<li>Lanikai Beach, Hawaii</li>\n</ol>\n<h3 id="responsibletourism">Responsible Tourism</h3>\n<blockquote>\n  <p>"We do not inherit the earth from our ancestors, we borrow it from our children." - Native American Proverb</p>\n</blockquote>\n<p>As travelers, it\'s important to be mindful of our impact on the environment. Here are some tips for responsible tourism:</p>\n<ul>\n<li>Choose eco-friendly accommodations and tours.</li>\n<li>Respect local customs and traditions.</li>\n<li>Reduce your plastic use by bringing a reusable water bottle and shopping bag.</li>\n<li>Support local businesses and communities.</li>\n</ul>\n<h3 id="coralreefs">Coral Reefs</h3>\n<p>Coral reefs are one of the most biodiverse ecosystems on the planet. Here are some Fas<wbr>ci<wbr>na<wbr>ting facts about these underwater wonderlands:</p>\n<ul>\n<li>Coral reefs cover less than 1% of the ocean floor but support over 25% of marine life.</li>\n<li>The Great Barrier Reef in Australia is the largest coral reef system in the world.</li>\n<li>Coral reefs are made up of tiny animals called polyps that secrete calcium carbonate to form a hard skeleton.</li>\n<li>Coral reefs are threatened by climate change, overfishing, and pollution.</li>\n</ul>\n</div>',
-                title: 'Ocean Vibes',
-                keywords: [
-                    'a',
-                    'beautiful',
-                    'sunset',
-                    'over',
-                    'the',
-                    'ocean',
-                    'p',
-                    'div',
-                    'style',
-                    'font',
-                    'family',
-                    'poppins',
-                    'sans',
-                    'serif',
-                    'h1',
-                    'id',
-                    'oceanvibes',
-                    'r',
-                    'vibe5s',
-                    'welcome',
-                    'to',
-                    'vibes',
-                    'website',
-                    'dedicated',
-                    'exploring',
-                    'wonders',
-                    'of',
-                    'our',
-                    'stunning',
-                    'wallpaper',
-                    'featuring',
-                    'sets',
-                    'tone',
-                    'for',
-                    'passion',
-                    'all',
-                    'things',
-                    'related',
-                    'sea',
-                    'h2',
-                    'aboutus',
-                    'about',
-                    'us',
-                    'at',
-                    'we',
-                    'are',
-                    'committed',
-                    'sharing',
-                    'love',
-                    'and',
-                    'knowledge',
-                    'with',
-                    'others',
-                    'offer',
-                    'variety',
-                    'resources',
-                    'including',
-                    'articles',
-                    'on',
-                    'marine',
-                    'life',
-                    'conservation',
-                    'efforts',
-                    'travel',
-                    'destinations',
-                    'much',
-                    'more',
-                    'userstories',
-                    'user',
-                    'stories',
-                    'community',
-                    'is',
-                    'made',
-                    'up',
-                    'people',
-                    'from',
-                    'walks',
-                    'who',
-                    'share',
-                    'common',
-                    'here',
-                    'some',
-                    'users',
-                    'ul',
-                    'li',
-                    'i',
-                    've',
-                    'always',
-                    'been',
-                    'fascinated',
-                    'by',
-                    'but',
-                    'never',
-                    'knew',
-                    'where',
-                    'start',
-                    'has',
-                    'given',
-                    'me',
-                    'wealth',
-                    'information',
-                    'inspiration',
-                    'explore',
-                    'this',
-                    'amazing',
-                    'world',
-                    'emily',
-                    '28',
-                    'have',
-                    'helped',
-                    'plan',
-                    'my',
-                    'dream',
-                    'trip',
-                    'hawaii',
-                    'can',
-                    't',
-                    'wait',
-                    'coral',
-                    'reefs',
-                    'see',
-                    'incredible',
-                    'mark',
-                    '35',
-                    'as',
-                    'an',
-                    'avid',
-                    'scuba',
-                    'diver',
-                    'appreciate',
-                    'dedication',
-                    'promoting',
-                    'responsible',
-                    'tourism',
-                    'it',
-                    's',
-                    'important',
-                    'protect',
-                    'so',
-                    'future',
-                    'generations',
-                    'enjoy',
-                    'too',
-                    'sarah',
-                    '42',
-                    'check',
-                    'out',
-                    'most',
-                    'popular',
-                    'href',
-                    'best',
-                    'beaches',
-                    '10',
-                    'in',
-                    'protecting',
-                    'oceans',
-                    'tips',
-                    'fas',
-                    'wbr',
-                    'ci',
-                    'na',
-                    'ting',
-                    'contactus',
-                    'contact',
-                    'do',
-                    'you',
-                    'questions',
-                    'or',
-                    'suggestions',
-                    'd',
-                    'hear',
-                    'reach',
-                    'mailto',
-                    'example',
-                    'com',
-                    'thank',
-                    'visiting',
-                    'let',
-                    'work',
-                    'together',
-                    'preserve',
-                    'beauty',
-                    'come',
-                    'hr',
-                    'h3',
-                    'bestbeaches',
-                    'ol',
-                    'anse',
-                    'source',
-                    'argent',
-                    'seychelles',
-                    'whitehaven',
-                    'beach',
-                    'australia',
-                    'navagio',
-                    'greece',
-                    'pink',
-                    'sands',
-                    'bahamas',
-                    'maya',
-                    'bay',
-                    'thailand',
-                    'tulum',
-                    'mexico',
-                    'matira',
-                    'french',
-                    'polynesia',
-                    'seven',
-                    'mile',
-                    'jamaica',
-                    'horseshoe',
-                    'bermuda',
-                    'lanikai',
-                    'responsibletourism',
-                    'blockquote',
-                    'not',
-                    'inherit',
-                    'earth',
-                    'ancestors',
-                    'borrow',
-                    'children',
-                    'native',
-                    'american',
-                    'proverb',
-                    'travelers',
-                    'be',
-                    'mindful',
-                    'impact',
-                    'environment',
-                    'choose',
-                    'eco',
-                    'friendly',
-                    'accommodations',
-                    'tours',
-                    'respect',
-                    'local',
-                    'customs',
-                    'traditions',
-                    'reduce',
-                    'your',
-                    'plastic',
-                    'use',
-                    'bringing',
-                    'reusable',
-                    'water',
-                    'bottle',
-                    'shopping',
-                    'bag',
-                    'support',
-                    'businesses',
-                    'communities',
-                    'coralreefs',
-                    'one',
-                    'biodiverse',
-                    'ecosystems',
-                    'planet',
-                    'facts',
-                    'these',
-                    'underwater',
-                    'wonderlands',
-                    'cover',
-                    'less',
-                    'than',
-                    '1',
-                    'floor',
-                    '25',
-                    'great',
-                    'barrier',
-                    'reef',
-                    'largest',
-                    'system',
-                    'tiny',
-                    'animals',
-                    'called',
-                    'polyps',
-                    'that',
-                    'secrete',
-                    'calcium',
-                    'carbonate',
-                    'form',
-                    'hard',
-                    'skeleton',
-                    'threatened',
-                    'climate',
-                    'change',
-                    'overfishing',
-                    'pollution',
-                ],
+                content: spaceTrim(`
+                
+                    <div style="font-family: Poppins, sans-serif;">
+                    <h1 id="oceanvibes">Ocean Vibes</h1>
+                    <p>Welcome to Ocean Vibes, a website dedicated to exploring the wonders of the ocean. Our stunning wallpaper featuring a beautiful sunset over the ocean sets the tone for our passion for all things related to the sea.</p>
+                    <h2 id="aboutus">About Us</h2>
+                    <p>At Ocean Vibes, we are committed to sharing our love and knowledge of the ocean with others. We offer a variety of resources, including articles on marine life, conservation efforts, travel destinations, and much more.</p>
+                    <h2 id="userstories">User Stories</h2>
+                    <p>Our community is made up of people from all walks of life who share a common love for the ocean. Here are some stories from our users:</p>
+                    <ul>
+                    <li>"I\'ve always been fascinated by the ocean, but never knew where to start. Ocean Vibes has given me a wealth of information and inspiration to explore this amazing world." - Emily, 28</li>
+                    <li>"The articles on Ocean Vibes have helped me plan my dream trip to Hawaii. I can\'t wait to explore the coral reefs and see all the incredible marine life." - Mark, 35</li>
+                    <li>"As an avid scuba diver, I appreciate the dedication Ocean Vibes has to promoting responsible tourism and conservation efforts. It\'s important to protect the ocean so future generations can enjoy it too." - Sarah, 42</li>
+                    </ul>
+                    <h2 id="resources">Resources</h2>
+                    <p>Check out some of our most popular articles and resources:</p>
+                    <ul>
+                    <li><a href="#best-beaches">10 Best Beaches in the World</a></li>
+                    <li><a href="#responsible-tourism">Protecting Our Oceans: Tips for Responsible Tourism</a></li>
+                    <li><a href="#coral-reefs">The Fas<wbr>ci<wbr>na<wbr>ting World of Coral Reefs</a></li>
+                    </ul>
+                    <h2 id="contactus">Contact Us</h2>
+                    <p>Do you have questions or suggestions? We\'d love to hear from you! Reach out to us at <a href="mailto:oceanvibes@example.com">oceanvibes@example.com</a>.</p>
+                    <p>Thank you for visiting Ocean Vibes. Let\'s work together to preserve the beauty of the ocean for generations to come 🐬🌊.</p>
+                    <hr>
+                    <h3 id="bestbeaches">Best Beaches</h3>
+                    <ol>
+                    <li>Anse Source d\'Argent, Seychelles</li>
+                    <li>Whitehaven Beach, Australia</li>
+                    <li>Navagio Beach, Greece</li>
+                    <li>Pink Sands Beach, Bahamas</li>
+                    <li>Maya Bay, Thailand</li>
+                    <li>Tulum Beach, Mexico</li>
+                    <li>Matira Beach, French Polynesia</li>
+                    <li>Seven Mile Beach, Jamaica</li>
+                    <li>Horseshoe Bay, Bermuda</li>
+                    <li>Lanikai Beach, Hawaii</li>
+                    </ol>
+                    <h3 id="responsibletourism">Responsible Tourism</h3>
+                    <blockquote>
+                    <p>"We do not inherit the earth from our ancestors, we borrow it from our children." - Native American Proverb</p>
+                    </blockquote>
+                    <p>As travelers, it\'s important to be mindful of our impact on the environment. Here are some tips for responsible tourism:</p>
+                    <ul>
+                    <li>Choose eco-friendly accommodations and tours.</li>
+                    <li>Respect local customs and traditions.</li>
+                    <li>Reduce your plastic use by bringing a reusable water bottle and shopping bag.</li>
+                    <li>Support local businesses and communities.</li>
+                    </ul>
+                    <h3 id="coralreefs">Coral Reefs</h3>
+                    <p>Coral reefs are one of the most biodiverse ecosystems on the planet. Here are some Fas<wbr>ci<wbr>na<wbr>ting facts about these underwater wonderlands:</p>
+                    <ul>
+                    <li>Coral reefs cover less than 1% of the ocean floor but support over 25% of marine life.</li>
+                    <li>The Great Barrier Reef in Australia is the largest coral reef system in the world.</li>
+                    <li>Coral reefs are made up of tiny animals called polyps that secrete calcium carbonate to form a hard skeleton.</li>
+                    <li>Coral reefs are threatened by climate change, overfishing, and pollution.</li>
+                    </ul>
+                    </div>
+                    
+                `),
             }),
-        ).toBe(`ocean-vibes-1s6e6kvountn`);
+        ).toBe(`ocean-vibes-2kcnh3w2bhgj`);
     });
 });
