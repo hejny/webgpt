fe0ada0d8899667f0c387500695d2af3fe199d0b
Pavol Hejny <me@pavolhejny.com>
2023-07-06 18:53:34 +0200

    Testing logs

--- src/components/MarkdownContent/HtmlContent.tsx
+++ src/components/MarkdownContent/HtmlContent.tsx
@@ -71,6 +71,7 @@ function HtmlContentEditable(props: Omit<HtmlContentProps, 'isEditable'>) {
         }
 
         if (element.innerHTML !== content) {
+            console.info(`!!! element.innerHTML = content`, element.innerHTML, content);
             element.innerHTML = content /* <- Here [3] */;
         }
     }, [content]);
