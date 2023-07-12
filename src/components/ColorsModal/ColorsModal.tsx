3f1b6375b690aeb2198b7c40be8240630fcf3e9b
Pavol Hejny <me@pavolhejny.com>
2023-07-12 19:02:50 +0200

    Cleanup of <ColorsModal/>

--- src/components/ColorsModal/ColorsModal.tsx
+++ src/components/ColorsModal/ColorsModal.tsx
@@ -1,8 +1,6 @@
 import '@uiw/react-markdown-editor/markdown-editor.css';
 import '@uiw/react-markdown-preview/markdown.css';
 import { debounce } from 'lodash';
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { classNames } from '../../utils/classNames';
 import { textColor } from '../../utils/color/operators/furthest';
@@ -11,18 +9,9 @@ import { useWallpaper } from '../../utils/hooks/useWallpaper';
 import { take } from '../../utils/take/take';
 import { ColorInput } from '../ColorInput/ColorInput';
 import { ImagineTag } from '../ImagineTag/ImagineTag';
-import { Modal, OpenModalLink } from '../Modal/Modal';
+import { Modal } from '../Modal/Modal';
 import styles from './ColorsModal.module.css';
 import { ColorsModalColorAlgoritm } from './ColorsModalColorAlgoritm';
-import { SaveBoardButton } from './CreateWallpaperButton/CreateWallpaperButton';
-
-const MarkdownEditor = dynamic(() => import('@uiw/react-markdown-editor').then((mod) => mod.default), { ssr: false });
-const ColorsModalDownloadButtons = dynamic(
-    () => import('./ColorsModalDownloadButtons').then(({ ColorsModalDownloadButtons }) => ColorsModalDownloadButtons),
-    {
-        loading: () => <p>Loading...</p>,
-    },
-);
 
 interface ColorsModalProps {}
 
@@ -71,72 +60,6 @@ export function ColorsModal(props: ColorsModalProps) {
                     </div>
                 ))}
             </div>
-
-            {/*
-            <div className={styles.section}>
-                <div>
-                    averageColor:
-                    <ColorBox value={wallpaper.colorStats.averageColor} />
-                </div>
-
-                {/*<pre>{JSON.stringify(wallpaper.colorStats, null, 4)}</pre>* /}
-            </div>
-            */}
-
-            <div className={styles.section}>
-                <MarkdownEditor
-                    className={styles.editor}
-                    value={wallpaper.content}
-                    onChange={(content) => {
-                        modifyWallpaper((modifiedWallpaper) => {
-                            modifiedWallpaper.content = content;
-                            modifiedWallpaper.saveStage = 'EDITED';
-                            return modifiedWallpaper;
-                        });
-                    }}
-                    // TODO: Hide fullscreen button
-                    // toolbarsFilter={(tool) => tool === 'fullscreen'}
-                />
-            </div>
-
-            <div className={styles.section}>
-                <ColorsModalDownloadButtons />
-                <OpenModalLink className={'button'} modal={'export'}>
-                    More Download Options
-                </OpenModalLink>
-                <button
-                    className={'button'}
-                    onClick={() => {
-                        (window as any).fooFunction();
-                    }}
-                >
-                    {/* TODO: !! Remove */}
-                    Invoke error
-                </button>
-
-                <SaveBoardButton parentWallpaperId={wallpaperId} {...wallpaper /* <- !!! Save UPDATED colorStats */}>
-                    Save
-                </SaveBoardButton>
-
-                <Link
-                    className={'button'}
-                    href={{
-                        pathname: '/[wallpaper]',
-                        query: {
-                            wallpaper: router.query.wallpaper,
-                        },
-                    }}
-                >
-                    Done
-                </Link>
-            </div>
         </Modal>
     );
 }
-
-/**
- * TODO: !!! Color of modal UI should be indipendent of wallpaper
- * TODO: !!! Fix unsaved changes
- * TODO: !!! [🧠] Split into info, edit and export part
- * TODO: !!! Allow to apply color-stats with different algorithms
- */
