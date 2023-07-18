49c7e2444fc4307eb40480bf9b98eb839d00fb35
Pavol Hejny <me@pavolhejny.com>
2023-07-19 00:41:25 +0200

    Try to better preload of random wallpaper

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -111,13 +111,10 @@ export function ControlPanel(props: ControlPanelProps) {
                         // Note: !!!!
                         /* not await */ fetch(`/${randomWallpaper.id}`);
 
-
- /* not await */ fetch(randomWallpaper.src);
+                        /* not await */ fetch(randomWallpaper.src);
 
                         // !!!! TODO: addEventListener before any await OR as onClick via react
                         element.addEventListener('click', async () => {
-                            await router.push(`/${randomWallpaper.id}`);
-
                             /*
                             TODO: !!!! Remove or uncomment + write the purpose
                             */
@@ -130,6 +127,8 @@ export function ControlPanel(props: ControlPanelProps) {
                                 colorToDataUrl(randomWallpaper.colorStats.averageColor),
                             );
                             headerWallpaperElement.removeAttribute('srcset');
+
+                            await router.push(`/${randomWallpaper.id}`);
                         });
                     }}
                     style={
