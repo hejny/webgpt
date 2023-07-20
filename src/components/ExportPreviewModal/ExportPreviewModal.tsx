46944022495c9510d3ba3d9fe5a60b30bd47085a
Pavol Hejny <me@pavolhejny.com>
2023-07-21 00:35:49 +0200

    Export preview

--- src/components/ExportPreviewModal/ExportPreviewModal.tsx
+++ src/components/ExportPreviewModal/ExportPreviewModal.tsx
@@ -1,13 +1,12 @@
 import '@uiw/react-markdown-editor/markdown-editor.css';
 import '@uiw/react-markdown-preview/markdown.css';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { exportAsHtml } from '../../export/exportAsHtml';
-import { exportAsZip } from '../../export/exportAsZip';
-import { induceFileDownload } from '../../export/utils/induceFileDownload';
 import { usePromise } from '../../utils/hooks/usePromise';
 import { useWallpaper } from '../../utils/hooks/useWallpaper';
 import { Modal } from '../Modal/Modal';
 import styles from './ExportPreviewModal.module.css';
+import { ObjectUrl } from './utils/ObjectUrl';
 
 interface ExportPreviewModalProps {}
 
@@ -16,59 +15,52 @@ interface ExportPreviewModalProps {}
  */
 export function ExportPreviewModal(props: ExportPreviewModalProps) {
     const [wallpaper] = useWallpaper();
-    const [publicUrl, setPublicUrl] = useState<null | URL>(null);
-    const exportedPromise = useMemo(() => {
-        if (publicUrl === null) {
-            return null;
+    const exportedPromise = useMemo(
+        () =>
+            /* not await */ exportAsHtml(wallpaper, {
+                stylesPlace: 'EXTERNAL',
+                publicUrl: new URL('https://example.com/'),
+            }),
+        [wallpaper],
+    );
+    const { value: exported } = usePromise(exportedPromise);
+
+    // --------------
+    // TODO: !!! useObjectUrl hook
+    const [indexUrl, setIndexUrl] = useState<null | URL>(null);
+    useEffect(() => {
+        if (!exported) {
+            return;
         }
 
-        return /* not await */ exportAsHtml(wallpaper, { stylesPlace: 'EXTERNAL', publicUrl });
-    }, [publicUrl, wallpaper]);
-    const { value: exported } = usePromise(exportedPromise);
+        for (const file of exported.files) {
+            const blob = new Blob([file.content], { type: 'text/html' });
+            const objectUrl = ObjectUrl.fromBlob(blob);
+
+            if (file.pathname === 'index.html') {
+                // TODO: !!! [🧠] How to link files together?
+                setIndexUrl(objectUrl.url);
+            }
+        }
+
+        /*
+        !!!!!
+        return () => {
+            objectUrl.destroy();
+        };
+        */
+    }, [exported]);
+    // --------------
 
     console.info('🔽', { exported });
 
     return (
         <Modal title={'Export preview'}>
-            <div className={styles.settings}>
-                <label className={styles.setting}>
-                    Your URL:&nbsp;&nbsp;
-                    <input
-                        onChange={(e) => {
-                            setPublicUrl(new URL(e.target.value));
-                        }}
-                        placeholder="https://www.your-awesome-project.com/"
-                        type="text"
-                    />
-                </label>
-
-                <label className={styles.setting}>
-                    <button
-                        className={'button'}
-                        disabled={publicUrl === null}
-                        onClick={async () => {
-                            if (!publicUrl) {
-                                alert('Please enter your URL');
-                                return;
-                            }
-
-                            // TODO: !!! Make registration here
-
-                            /* not await */ induceFileDownload(await exportAsZip(wallpaper, { publicUrl }));
-                        }}
-                    >
-                        Download as .zip
-                    </button>
-                </label>
-            </div>
-
-            <iframe src=""></iframe>
-            {/* <FilesPreview files={exported} /> */}
+            {!indexUrl ? `Loading...` : <iframe className={styles.preview} src={indexUrl.href} />}
         </Modal>
     );
 }
 
 /**
- * TODO: !!! This is unused - use or remove
- * TODO: !!! DRY with ExportModal
+ * TODO: Maybe make a component <FilesPreview files={exported} />
  */
