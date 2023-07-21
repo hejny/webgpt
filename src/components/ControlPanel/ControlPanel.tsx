585955e731be855f366734a2f679c01d63abc9bf
Pavol Hejny <me@pavolhejny.com>
2023-07-21 10:04:21 +0200

    Move code to RandomWallpaperButton

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,19 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import type { RandomWallpaperResponse } from '../../pages/api/random-wallpaper';
 import { classNames } from '../../utils/classNames';
-import { colorToDataUrl } from '../../utils/color/utils/colorToDataUrl';
 import { computeWallpaperUriid } from '../../utils/computeWallpaperUriid';
 import { LikedStatus } from '../../utils/hooks/useLikedStatusOfCurrentWallpaper';
 import { useWallpaper } from '../../utils/hooks/useWallpaper';
-import { hydrateWallpaper, serializeWallpaper } from '../../utils/hydrateWallpaper';
+import { serializeWallpaper } from '../../utils/hydrateWallpaper';
 import { IWallpaper } from '../../utils/IWallpaper';
 import { getSupabaseForBrowser } from '../../utils/supabase/getSupabaseForBrowser';
 import { provideClientId } from '../../utils/supabase/provideClientId';
 import { parseKeywordsFromWallpaper } from '../Gallery/GalleryFilter/utils/parseKeywordsFromWallpaper';
 import styles from './ControlPanel.module.css';
 import { ControlPanelLikeButtons } from './ControlPanelLikeButtons';
+import { RandomWallpaperButton } from './RandomWallpaperButton';
 
 interface ControlPanelProps {
     randomWallpaper: IWallpaper;
@@ -89,63 +88,7 @@ export function ControlPanel(props: ControlPanelProps) {
             )}
 
             {wallpaper.saveStage === 'SAVED' && <ControlPanelLikeButtons />}
-
-            {wallpaper.saveStage === 'SAVED' && (
-                <button
-                    // TODO: !!! Go ACRY through randomWallpaper and cleanup old stuff
-
-                    /* Note: randomWallpaper image is already prerendered thare -> [🤰] <- !!!! Fix or remove + [🧠] do we want to prefetch random wallpaper, if yes, do it here */
-                    className={classNames(/*'button',*/ styles.button)}
-                    title="Show me another one"
-                    ref={async (element) => {
-                        if (!element) {
-                            return;
-                        }
-
-                        const response = await fetch('/api/random-wallpaper');
-                        const { randomWallpaper: randomWallpaperSerialized } =
-                            (await response.json()) as RandomWallpaperResponse;
-                        const randomWallpaper = hydrateWallpaper(randomWallpaperSerialized);
-                        console.info(`🎲 Pre-fetching next random wallpaper`, { randomWallpaper });
-
-                        // Note: !!!!
-                        /* not await */ fetch(`/${randomWallpaper.id}`);
-                        /* not await */ fetch(randomWallpaper.src);
-
-                        // !!!! TODO: addEventListener before any await OR as onClick via react
-                        element.addEventListener('click', async () => {
-                            /*
-                            TODO: !!!! Remove or uncomment + write the purpose
-                            */
-
-                            const headerWallpaperElement = document.getElementById(
-                                'HeaderWallpaper' /* <- TODO: Some system for global css classes */,
-                            )!;
-                            headerWallpaperElement.setAttribute(
-                                'src',
-                                colorToDataUrl(randomWallpaper.colorStats.averageColor),
-                            );
-                            headerWallpaperElement.removeAttribute('srcset');
-
-                            await router.push(`/${randomWallpaper.id}`);
-                        });
-                    }}
-                    style={
-                        {
-                            // ...minorButtonStyle,
-                            // TODO: Better or delete
-                            // backgroundColor: randomWallpaper.colorStats.averageColor.toHex(),
-                            // color: randomWallpaper.colorStats.averageColor.then(textColor).toHex(),
-                        }
-                    }
-                >
-                    {/* TODO: This should have more role like next not random */}
-
-                    <Image alt="🎲" src="/icons/openmoji/1F3B2.svg" width={40} height={40} /* <-[🧥] */ />
-                    {/*  <MarkdownContent content="🎲" isUsingOpenmoji /> */}
-                </button>
-            )}
-
+            {wallpaper.saveStage === 'SAVED' && <RandomWallpaperButton />}
             {wallpaper.saveStage === 'SAVED' && (
                 <Link
                     href={{
---
+++ src/components/ControlPanel/RandomWallpaperButton.tsx
@@ -0,0 +1,64 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { RandomWallpaperResponse } from '../../pages/api/random-wallpaper';
+import { classNames } from '../../utils/classNames';
+import { colorToDataUrl } from '../../utils/color/utils/colorToDataUrl';
+import { hydrateWallpaper } from '../../utils/hydrateWallpaper';
+import styles from './ControlPanel.module.css';
+
+export function RandomWallpaperButton() {
+    const router = useRouter();
+
+    return (
+        <button
+            // TODO: !!! Go ACRY through randomWallpaper and cleanup old stuff
+
+            /* Note: randomWallpaper image is already prerendered thare -> [🤰] <- !!!! Fix or remove + [🧠] do we want to prefetch random wallpaper, if yes, do it here */
+            className={classNames(/*'button',*/ styles.button)}
+            title="Show me another one"
+            ref={async (element) => {
+                if (!element) {
+                    return;
+                }
+
+                const response = await fetch('/api/random-wallpaper');
+                const { randomWallpaper: randomWallpaperSerialized } =
+                    (await response.json()) as RandomWallpaperResponse;
+                const randomWallpaper = hydrateWallpaper(randomWallpaperSerialized);
+                console.info(`🎲 Pre-fetching next random wallpaper`, { randomWallpaper });
+
+                // Note: !!!!
+                /* not await */ fetch(`/${randomWallpaper.id}`);
+                /* not await */ fetch(randomWallpaper.src);
+
+                // !!!! TODO: addEventListener before any await OR as onClick via react
+                element.addEventListener('click', async () => {
+                    /*
+                TODO: !!!! Remove or uncomment + write the purpose
+                */
+
+                    const headerWallpaperElement = document.getElementById(
+                        'HeaderWallpaper' /* <- TODO: Some system for global css classes */,
+                    )!;
+                    headerWallpaperElement.setAttribute('src', colorToDataUrl(randomWallpaper.colorStats.averageColor));
+                    headerWallpaperElement.removeAttribute('srcset');
+
+                    await router.push(`/${randomWallpaper.id}`);
+                });
+            }}
+            style={
+                {
+                    // ...minorButtonStyle,
+                    // TODO: Better or delete
+                    // backgroundColor: randomWallpaper.colorStats.averageColor.toHex(),
+                    // color: randomWallpaper.colorStats.averageColor.then(textColor).toHex(),
+                }
+            }
+        >
+            {/* TODO: This should have more role like next not random */}
+
+            <Image alt="🎲" src="/icons/openmoji/1F3B2.svg" width={40} height={40} /* <-[🧥] */ />
+            {/*  <MarkdownContent content="🎲" isUsingOpenmoji /> */}
+        </button>
+    );
+}
