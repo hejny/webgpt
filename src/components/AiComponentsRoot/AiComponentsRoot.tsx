fa64e9395a554cb453aff10b48683092f39f7afb
Pavol Hejny <me@pavolhejny.com>
2023-07-30 17:38:14 +0200

    Add support for dynamically rendering components in AiComponentsRoot.tsx
    This commit enables the dynamic rendering of components in AiComponentsRoot by adding a new functionality to iterate through each component and render them accordingly.

--- src/components/AiComponentsRoot/AiComponentsRoot.tsx
+++ src/components/AiComponentsRoot/AiComponentsRoot.tsx
@@ -77,6 +77,7 @@ export function AiComponentsRoot(props: AiComponentsRootProps) {
                                                                                   - OR just Put a real name on the function
                                                                         */
                                                 })(componentElement);
+                                                break;
                                         `,
                                     )
                                     .join('\n\n')}
