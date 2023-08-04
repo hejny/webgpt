bb5922c765160f90e77d8cb291902bc16b212648
Pavol Hejny <me@pavolhejny.com>
2023-08-04 18:28:21 +0200

    Use rgb color to debug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { StaticAppHead } from '../components/AppHead/StaticAppHead';
-import { FooterSection } from '../components/Footer/Footer';
 import { NoSsr } from '../components/NoSsr/NoSsr';
-import { Section } from '../components/Section/Section';
-import { TiledBackground } from '../components/TiledBackground/TiledBackground';
 import { WelcomeWallpaperShuffle } from '../components/WelcomeWallpaperShuffle/WelcomeWallpaperShuffle';
-import styles from '../styles/static.module.css';
 
 interface HomePageProps {}
 
@@ -14,30 +10,11 @@ export default function HomePage(props: HomePageProps) {
         <>
             <StaticAppHead subtitle="1-2i" />
 
-            <div className={styles.page}>
-                <header>
-                    {/* TODO: Do some system for multiple pages */}
-                    {/* <CaveSection /> */}
-                </header>
-                <div className={styles.background}>
-                    {/* TODO: Do some system for multiple pages */}
-                    <TiledBackground />
-                </div>
-                <main>
-                    {/* <WelcomeSection variant="SIDEPAGE" /> */}
-                    <Section>
-                        <h2>1-2i</h2>
-                        <NoSsr>
-                            <WelcomeWallpaperShuffle />
-                            <WelcomeWallpaperShuffle />
-                            <WelcomeWallpaperShuffle />
-                        </NoSsr>
-                    </Section>
-                </main>
-                <footer>
-                    <FooterSection />
-                </footer>
-            </div>
+            <main>
+                <NoSsr>
+                    <WelcomeWallpaperShuffle />
+                </NoSsr>
+            </main>
         </>
     );
 }
@@ -51,6 +28,7 @@ export async function getStaticProps({ locale }: { locale: string }) {
 }
 
 /**
+ * TODO: !!!! Make fallback for sharing and search (Like OG images etc...)
  * TODO: Go to last wallpaper
  * TODO: Make shuffle between wallpapers
  */
