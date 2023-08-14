379e6e647a2f4a3814fac466f54a09c658482fbb
Pavol Hejny <me@pavolhejny.com>
2023-08-15 01:18:38 +0200

    Remove ids from sections
    to use then in <Markdown> heading hashes

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -11,32 +11,13 @@ import styles from './Section.module.css';
  */
 interface SectionProps {
     children: ReactNode;
-
-    /**
-     * Unique ID of the section which can be used for hash link
-     * Note: It is case-insensitive and will be converted to lowercase
-     */
-    id?: string;
     className?: string_css_class;
 }
 
 /**
  * @@@
  */
-export function Section({ id, children, className }: SectionProps) {
-    // TODO: [0] Do or comment> const [headChild, ...restChildren] = children;
-
-    id = id?.toLocaleLowerCase();
-
-    return (
-        <div
-            className={classNames(styles.section, className)}
-            {
-                ...{ id } /* <- TODO: !!! Remove ids from sections (to use then in <Markdown> heading hashes) */
-            }
-        >
-            {/* [0] <a href={`#${id}`}>{headChild}</a>{restChildren}*/}
-            {children}
-        </div>
-    );
+export function Section(props: SectionProps) {
+    const { children, className } = props;
+    return <div className={classNames(styles.section, className)}>{children}</div>;
 }
