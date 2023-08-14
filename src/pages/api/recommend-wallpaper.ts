6f1c371f8a28746d7474d9721699b02fd1e3061e
Pavol Hejny <me@pavolhejny.com>
2023-08-14 17:20:02 +0200

    📤 Move important TODOs to pull request
    This will be immediatelly reverted into a branch:
    feature/recommendation-system
    
    And used in pull request:
    ⭐ Recommendation system

--- src/pages/api/recommend-wallpaper.ts
+++ src/pages/api/recommend-wallpaper.ts
@@ -41,7 +41,6 @@ export default async function recommendWallpaperHandler(
                 .eq('author', author)
                 .eq('likedStatus', likedStatus)
                 .order('createdAt', { ascending: false })
-                // <- TODO: !!!! [🤺][🧠] Take ONLY current reactions NOT overwritten ones
                 .limit(10 /* <- TODO:  [🤺] Tweak this number */);
 
             const likeness = likedStatusToLikeness(likedStatus);
