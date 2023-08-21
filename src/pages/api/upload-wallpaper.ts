62c9ab049680cf6e7dfebcb99b002993991a5d06
Pavol Hejny <me@pavolhejny.com>
2023-08-22 01:08:08 +0200

    Upload as a formdata

--- src/pages/api/upload-wallpaper.ts
+++ src/pages/api/upload-wallpaper.ts
@@ -1,7 +1,8 @@
 import formidable from 'formidable';
-import { readFile, writeFile } from 'fs/promises';
+import { readFile } from 'fs/promises';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { join } from 'path';
+import { CDN } from '../../../config';
+import { generateUserWallpaperCdnKey } from '../../utils/cdn/utils/generateUserWallpaperCdnKey';
 
 import { string_url } from '../../utils/typeAliases';
 
@@ -48,63 +49,17 @@ export default async function uploadWallpaperHandler(
         return response.status(400).json({ message: 'Only image files are allowed' } as any);
     }
 
-    console.log('wallpaper', wallpaper);
-    console.log('wallpaper.filepath', wallpaper.filepath);
-
     const wallpaperBuffer = await readFile(wallpaper.filepath);
 
-    await writeFile(join(process.cwd(), 'test.jpg'), wallpaperBuffer);
-
-    /*
-    console.log('request.headers', request.headers);
-    const wallpaperAsBlob1 = new Blob([request.body], { type: request.headers['content-type'] as string_mime_type });
-    const wallpaperAsBlob2 = request.read();
-
-    request.on('readable', async () => {
-        const data = request.read();
-
-        console.log('data', data);
-
-        await writeFile(join(process.cwd(), 'test-data.jpg'), data).catch(() => {});
-        await writeFile(join(process.cwd(), 'test-data-binary.jpg'), data, 'binary').catch(() => {});
-        await writeFile(join(process.cwd(), 'test-data-ascii.jpg'), data, 'ascii').catch(() => {});
-        await writeFile(join(process.cwd(), 'test-data-utf8.jpg'), data, 'utf8').catch(() => {});
-
-        const datablob = new Blob([data], { type: request.headers['content-type'] as string_mime_type });
-        console.log('datablob', datablob);
-
-        await writeFile(join(process.cwd(), 'test-datablob.jpg'), data).catch(() => {});
-        await writeFile(join(process.cwd(), 'test-datablob-binary.jpg'), data, 'binary').catch(() => {});
-        await writeFile(join(process.cwd(), 'test-datablob-ascii.jpg'), data, 'ascii').catch(() => {});
-        await writeFile(join(process.cwd(), 'test-datablob-utf8.jpg'), data, 'utf8').catch(() => {});
-    });
-
-    console.log({ wallpaperAsBlob1, wallpaperAsBlob2 });
-
-    const wallpaperAsBlob = wallpaperAsBlob1;
-
-    const wallpaperAsbuffer = Buffer.from(await wallpaperAsBlob.arrayBuffer());
-
-    // save file to disk
-    await writeFile(join(process.cwd(), 'test-buffer.jpg'), wallpaperAsbuffer);
-    // writeFile(join(process.cwd(),'test-file.jpg'), wallpaperAsBlob1);
-    await writeFile(join(process.cwd(), 'test-body.jpg'), request.body);
-    await writeFile(join(process.cwd(), 'test-body-binary.jpg'), request.body, 'binary');
-    await writeFile(join(process.cwd(), 'test-body-ascii.jpg'), request.body, 'ascii');
-    await writeFile(join(process.cwd(), 'test-body-utf8.jpg'), request.body, 'utf8');
-
-    const key = generateUserWallpaperCdnKey(request.body);
+    const key = generateUserWallpaperCdnKey(wallpaperBuffer);
     await CDN.setItem(key, {
-        type: wallpaperAsBlob.type,
-        data: request.body,
+        type: wallpaper.mimetype,
+        data: wallpaperBuffer,
     });
 
     const wallpaperUrl = CDN.getItemUrl(key).href;
-    console.log({ wallpaperUrl });
 
     return response.status(201).json({ wallpaperUrl } satisfies UploadWallpaperResponse);
-
-    */
 }
 
 /**
