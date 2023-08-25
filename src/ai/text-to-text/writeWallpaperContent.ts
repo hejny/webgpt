4db25fbe8b0936e4e2b381a827b6c2ce9d162da5
Pavol Hejny <me@pavolhejny.com>
2023-08-25 17:55:18 +0200

    Add better mocked content in writeWallpaperContent

--- src/ai/text-to-text/writeWallpaperContent.ts
+++ src/ai/text-to-text/writeWallpaperContent.ts
@@ -30,10 +30,41 @@ export async function writeWallpaperContent(
 
     return spaceTrim(
         (block) => `
+    
+            <!--font:Barlow Condensed-->
 
             # ${block(responseText)}
-    
-            Content !!!
+
+            Welcome to our website dedicated to space exploration and adventure. Our pixel art background featuring a spacecraft adds a playful and nostalgic touch to your browsing experience.
+
+            ## Our Mission
+
+            Our mission is to inspire curiosity and wonder about the universe through engaging content and immersive experiences. Whether you're a seasoned astronomer or just starting to explore the cosmos, we have something for everyone.
+
+            ## Features
+
+            - **Articles**: Our team of writers and experts provide in-depth articles on the latest discoveries, space missions, and more.
+            - **Interactive Maps**: Explore the solar system and beyond with our interactive maps that let you zoom in and out, and learn about different celestial bodies.
+            - **User Stories**: Read stories from other space enthusiasts who share their experiences and passion for space exploration.
+            - **Resources**: Access a variety of resources including books, documentaries, and websites to deepen your knowledge and understanding of space.
+
+            ## References
+
+            We've compiled a list of references that you might find interesting:
+
+            - [NASA](#) - The official website of the National Aeronautics and Space Administration.
+            - [Space.com](#) - A website dedicated to space news, science, and exploration.
+            - [Astronomy Magazine](#) - A magazine that covers astronomy and space science.
+
+            ## Contact Us
+
+            If you have any questions or feedback, please don't hesitate to contact us. We'd love to hear from you!
+
+            - Email: info@spaceadventure.com
+            - Twitter: [@spaceadventure](#)
+            - Facebook: [Space Adventure](#)
+
+            Join us on our space adventure and explore the universe with our pixel art background. Let your imagination soar!
 
             <!-- ${block(metadataText)} -->
         
@@ -42,6 +73,7 @@ export async function writeWallpaperContent(
 }
 
 /**
+ * TODO: !! Put step by step instructions how the content is generated in footer comment
  * TODO: [👸] Use in generate-wallpapers-content and DRY
  * TODO: [👮‍♀️] In this repository is used both 'chatgpt' and 'openai' NPM packages - use just 'openai' in future and in scripts use the common utils
  */
