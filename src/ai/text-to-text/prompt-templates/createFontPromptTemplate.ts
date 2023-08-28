ff54eae014dc4a3c58e6002f60625ab7b37b099a
Pavol Hejny <me@pavolhejny.com>
2023-08-28 15:17:43 +0200

    Annotate createFontPromptTemplate

--- src/ai/text-to-text/prompt-templates/createFontPromptTemplate.ts
+++ src/ai/text-to-text/prompt-templates/createFontPromptTemplate.ts
@@ -1,9 +1,15 @@
 import spaceTrim from 'spacetrim';
 import { FONTS } from '../../../../config';
+import { string_chat_prompt } from '../../../utils/typeAliases';
 
-// TODO: !!! Annotate
-
-export function createFontPromptTemplate() {
+/**
+ * Generates a template for figuring out best fitting font for the website.
+ *
+ * Note: This should be a second message in the conversation after the createTitlePromptTemplate
+ *
+ * @returns A template that can be used with ChatGPT to detect best fitting font for the website.
+ */
+export function createFontPromptTemplate(): string_chat_prompt {
     return spaceTrim(
         (block) =>
             `
