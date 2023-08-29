64c332079373a94bd04abc82b8084c693fccfb47
Pavol Hejny <me@pavolhejny.com>
2023-08-29 16:32:47 +0200

    TODOs

--- src/components/UploadNewWallpaper/UploadNewWallpaper.tsx
+++ src/components/UploadNewWallpaper/UploadNewWallpaper.tsx
@@ -71,11 +71,12 @@ export function UploadNewWallpaper() {
 }
 
 /**
- * TODO: !! Error handling in worker
- * TODO: Send progress from worker to UI
+ * TODO: Show progress checkmarks
+ * TODO: !!! Error handling in worker
+ * TODO: !!! Send progress from worker to UI
  * TODO: !!! Speed up the computation of colorstats
  * TODO: Detect image content and write content dynamically just for this image
- * TODO: Compute colorstats in worker
+ * TODO: !!! Compute ONLY colorstats in worker, rest here - split workers into multiple parts
  * TODO: Upload image and Compute colorstats in parallel + remove the comment blocks
  * TODO: Maybe it is not very efficient to first convert image to dataurl and create image from the dataurl - maybe just createImageFromFile / createImageFromBlob
  * TODO: !! It Should be possible to list private wallpapers via getSupabaseForBrowser().from('Wallpaper').select('*').eq('isPublic', false)
