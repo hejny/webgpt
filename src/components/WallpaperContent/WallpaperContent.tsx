af0e6f45cbd7061fdd117662659c68c22d4563b8
Pavol Hejny <me@pavolhejny.com>
2023-08-29 19:13:25 +0200

    Move important TODOs to feature branch

--- src/components/WallpaperContent/WallpaperContent.tsx
+++ src/components/WallpaperContent/WallpaperContent.tsx
@@ -131,7 +131,7 @@ export function WallpaperContentSection() {
 
 /**
  * TODO: [👩‍🦰] Allow to change fonts in <WallpaperContentSection/> or <Content/> or <HtmlContent/>
- * TODO: [👨‍🦰] !!! Show editable hint in <WallpaperContentSection/> or <Content/> or <HtmlContent/>
+ * TODO: [👨‍🦰] Show editable hint in <WallpaperContentSection/> or <Content/> or <HtmlContent/>
  * TODO: Maybe split header and content font
  * TODO: isHashUsed + test it and put into menu + some way to return existing hashes
  * TODO: [🎐] Some markdown can not be converted back from html - use fallback to pure html content
