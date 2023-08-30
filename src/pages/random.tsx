de4397bc8bf5f888068c74220af461f575baf447
Pavol Hejny <me@pavolhejny.com>
2023-08-30 02:33:27 +0200

    Fix types

--- src/pages/random.tsx
+++ src/pages/random.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { StaticAppHead } from '../components/AppHead/StaticAppHead';
 import { RandomWallpaperManager } from '../components/ControlPanel/RandomWallpaper/RandomWallpaperManager';
-import { WorkInProgress } from '../components/WorkInProgress/WorkInProgress';
+import { TaskInProgress } from '../components/TaskInProgress/TaskInProgress';
 import styles from '../styles/static.module.css';
 
 export default function RandomPage() {
@@ -29,7 +29,7 @@ export default function RandomPage() {
                     >
                         1-2i
                     </h1>
-                    <WorkInProgress />
+                    <TaskInProgress />
                 </main>
             </div>
         </>
