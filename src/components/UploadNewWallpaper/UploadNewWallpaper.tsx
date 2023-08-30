bf9b951e5f5a93e442a7d4c8adac33adfd161ccc
Pavol Hejny <me@pavolhejny.com>
2023-08-30 02:32:27 +0200

    setWorking(false) after job fail

--- src/components/UploadNewWallpaper/UploadNewWallpaper.tsx
+++ src/components/UploadNewWallpaper/UploadNewWallpaper.tsx
@@ -33,6 +33,7 @@ export function UploadNewWallpaper() {
                         }
 
                         alert(error.message);
+                        setWorking(false);
                     }
                 }}
             >
