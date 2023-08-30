70ef0d6d187d0420f3c2af05d62875cb89a8a3fd
Pavol Hejny <me@pavolhejny.com>
2023-08-30 02:05:15 +0200

    TODOs

--- src/components/TaskInProgress/TaskInProgress.tsx
+++ src/components/TaskInProgress/TaskInProgress.tsx
@@ -31,7 +31,8 @@ export function TaskInProgress() {
 }
 
 /**
- * TODO: !!! Initial rotate effect
+ * TODO: !!! Torus Graph: Move up
+ * TODO: !!! Torus Graph: Initial rotate effect
  * TODO: !!! Pass here TaskProgress and show
  * TODO: Play with shape and camera angle
  * TODO: Size of babylonjs in bundle - maybe prerecord as video
