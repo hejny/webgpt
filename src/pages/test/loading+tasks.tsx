980fb77967dbe9e7fa9914de4df4a5e01dea94b9
Pavol Hejny <me@pavolhejny.com>
2023-08-31 17:53:30 +0200

    Fix types

--- src/pages/test/loading+tasks.tsx
+++ src/pages/test/loading+tasks.tsx
@@ -108,7 +108,7 @@ export default function TestLoadingPage() {
             }
         });
 
-        return () => registration.destroy();
+        return () => void registration.destroy();
     });
 
     return (
