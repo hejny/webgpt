75f6407a9a2cb29846057bd93835849c46e15fef
Pavol Hejny <me@pavolhejny.com>
2023-09-08 09:03:54 +0200

    Bigger CopilotPanel

--- src/components/WallpaperEditing/WallpaperEditing.tsx
+++ src/components/WallpaperEditing/WallpaperEditing.tsx
@@ -3,7 +3,6 @@ import { ControlPanel } from '../../components/ControlPanel/ControlPanel';
 import { ExportModal } from '../../components/ExportModal/ExportModal';
 import { useSsrDetection } from '../../utils/hooks/useSsrDetection';
 import { ColorsModal } from '../ColorsModal/ColorsModal';
-import { ColorsPanel } from '../ColorsPanel/ColorsPanel';
 import { CopilotPanel } from '../CopilotPanel/CopilotPanel';
 import { ExportCodeModal } from '../ExportCodeModal/ExportCodeModal';
 import { ExportPreviewModal } from '../ExportPreviewModal/ExportPreviewModal';
@@ -31,13 +30,14 @@ export function WallpaperEditing() {
             {/* Note: <EditModal/> was removed in commit a4a37573299fa262ee335ecb1a5b480c409f8627 */}
 
             {isReady && !isServerRender && <ControlPanel />}
-            {isReady && !isServerRender && <ColorsPanel />}
+            {/*isReady && !isServerRender && <ColorsPanel />*/}
             {isReady && !isServerRender && <CopilotPanel />}
         </>
     );
 }
 
 /**
+ * TODO: [🧠] !!! How to show <ColorsPanel />
  * TODO: !! Lazy load modals
  * TODO: [🧠] This is not a section nor a component - figure out where to put it
  */
