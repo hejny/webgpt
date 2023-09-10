f6b3f218d8a7de8321e953228f6b7d7432e1270b
Pavol Hejny <me@pavolhejny.com>
2023-09-10 18:11:46 +0200

    useRole + useScenario

---
+++ src/utils/hooks/useRole.ts
@@ -0,0 +1,60 @@
+import { normalizeToKebabCase, normalizeTo_SCREAMING_CASE } from 'n12';
+import { useRouter } from 'next/router';
+import spaceTrim from 'spacetrim';
+import { TupleToUnion } from 'type-fest';
+import { useSsrDetection } from './useSsrDetection';
+
+const ROLES = ['VISITOR', 'OWNER', 'OWNER_AS_VISITOR'] as const;
+
+/**
+ * Role determines what user can do with the app and how the app behaves
+ * There are 3 roles:
+ * - "VISITOR" user can only view the app and won't be able to change anything or even see the controls
+ * - "OWNER" user can edit the app and see the controls
+ * - "OWNER_AS_VISITOR" simmilar to "VISITOR" but with tools for turning into "OWNER"
+ */
+export type Role = TupleToUnion<typeof ROLES>;
+
+export const DEFAULT_ROLE: Role = 'OWNER_AS_VISITOR';
+
+/**
+ * Hook for getting current role from GET params
+ *
+ * @returns Current role from GET params
+ */
+export function useRole(): Role {
+    const isServerRender = useSsrDetection();
+    const router = useRouter();
+
+    if (isServerRender) {
+        return DEFAULT_ROLE;
+    }
+
+    if (typeof router.query.role !== 'string') {
+        // This is a situation when there is no ?role=... in GET params
+        return DEFAULT_ROLE;
+    }
+
+    const role = normalizeTo_SCREAMING_CASE(router.query.role);
+
+    if (!ROLES.includes(role)) {
+        throw new Error(
+            spaceTrim(
+                (block) => `
+                    Invalid role in GET params "?role=${router.query.mode}"
+
+                    You can use one of these roles:
+                    ${block(ROLES.map((role) => `- "${normalizeToKebabCase(role)}"`).join('\n'))}
+                `,
+            ),
+        );
+    }
+
+    return role as Role;
+}
+
+/**
+ * TODO: Validate here the ownership
+ * TODO: !! Some way how to toggle VISITOR role from UI
+ * TODO: [🧠][🍛] There is a collision between role and <a role="..." /> html attribute
+ */
