5a85b12ca40f06e7a86e390170c83ddec3b1d908
Pavol Hejny <me@pavolhejny.com>
2023-09-13 14:33:05 +0200

    Draft

---
+++ src/components/EditContentModal/EditContentModal.tsx
@@ -0,0 +1,51 @@
+import '@uiw/react-markdown-editor/markdown-editor.css';
+import '@uiw/react-markdown-preview/markdown.css';
+import { useMemo } from 'react';
+import { exportAsHtml } from '../../export/exportAsHtml';
+import { useCurrentWallpaper } from '../../utils/hooks/useCurrentWallpaper';
+import { usePromise } from '../../utils/hooks/usePromise';
+import { Files } from '../Files/00-FilesPreview';
+import { Modal } from '../Modal/00-Modal';
+
+/**
+ * Renders the modal for exporting wallpaper page as code
+ */
+export function EditContentModal() {
+    const [wallpaper] = useCurrentWallpaper();
+
+    /**
+     * Memoized promise for exporting wallpaper as HTML
+     *
+     * @type {Promise<any>}
+     */
+    const exportedPromise = useMemo(
+        () =>
+            /* not await */ exportAsHtml(wallpaper, {
+                stylesPlace: 'EXTERNAL',
+                publicUrl: new URL('https://example.com/'),
+            }),
+        [wallpaper],
+    );
+    const { value: exported } = usePromise(exportedPromise);
+
+    console.info('🔽', { exported });
+
+    return (
+        <Modal title={'Export in code editor'}>
+            <Files
+                files={
+                    exported
+                        ? exported.files
+                        : [
+                              {
+                                  type: 'other',
+                                  mimeType: 'text/markdown',
+                                  pathname: 'README.md',
+                                  content: `Select your URL and download the project. Then, upload it to your hosting.`,
+                              },
+                          ]
+                }
+            />
+        </Modal>
+    );
+}
