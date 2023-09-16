638c5a8963728b58096b01f642f6349ec348fdb1
Pavol Hejny <me@pavolhejny.com>
2023-09-16 13:54:54 +0200

    Text: Pick a scenario to create a your website:

--- src/components/Scenarios/Scenarios.tsx
+++ src/components/Scenarios/Scenarios.tsx
@@ -7,32 +7,35 @@ import styles from './Scenarios.module.css';
  */
 export function Scenarios() {
     return (
-        <ul className={styles.Scenarios}>
-            <li>
-                <Link href="/random">
-                    <CreateZone className={styles.scenario}>
-                        <h2>Nothing</h2>
-                        Pick web from gallery of pre-prepared designg
-                    </CreateZone>
-                </Link>
-            </li>
-            <li>
-                <Link href="/new/from-prompt">
-                    <CreateZone className={styles.scenario}>
-                        <h2>Idea</h2>
-                        To describe and generate your website
-                    </CreateZone>
-                </Link>
-            </li>
-            <li>
-                <Link href="/new/from-image">
-                    <CreateZone className={styles.scenario}>
-                        <h2>Image</h2>
-                        To upload and generate your website
-                    </CreateZone>
-                </Link>
-            </li>
-        </ul>
+        <div className={styles.Scenarios}>
+            Pick a scenario to <b>create a your website</b>:
+            <ul>
+                <li>
+                    <Link href="/random">
+                        <CreateZone className={styles.scenario}>
+                            <h2>Nothing</h2>
+                            Pick web from gallery of pre-prepared designg
+                        </CreateZone>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/new/from-prompt">
+                        <CreateZone className={styles.scenario}>
+                            <h2>Idea</h2>
+                            To describe and generate your website
+                        </CreateZone>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/new/from-image">
+                        <CreateZone className={styles.scenario}>
+                            <h2>Image</h2>
+                            To upload and generate your website
+                        </CreateZone>
+                    </Link>
+                </li>
+            </ul>
+        </div>
     );
 }
 
