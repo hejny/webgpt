a4c9b1da1ef99286062b352fbf9e3cd2fe0bdb95
Pavol Hejny <me@pavolhejny.com>
2023-09-24 15:08:07 +0200

    PromptTemplatePipeline methods

--- src/ai/text-to-text/prompt-templates/lib/src/classes/PromptTemplatePipeline.ts
+++ src/ai/text-to-text/prompt-templates/lib/src/classes/PromptTemplatePipeline.ts
@@ -21,9 +21,33 @@ export class PromptTemplatePipeline {
             promptTemplate: PromptTemplate<any /* <- TODO: Get rid of any */>;
             resultingParamName: string;
         }>,
-    ) {}
+    ) {
+        if (promptTemplates.length === 0) {
+            throw new Error(`Prompt template pipeline must have at least one prompt template`);
+        }
+    }
+
+    public get entryPromptTemplate(): PromptTemplate<any /* <- TODO: Get rid of any */> {
+        return this.promptTemplates[0]!.promptTemplate;
+    }
+
+    public getFollowingPromptTemplate(
+        curentPromptTemplate: PromptTemplate<any /* <- TODO: Get rid of any */>,
+    ): PromptTemplate<any /* <- TODO: Get rid of any */> | null {
+        const index = this.promptTemplates.findIndex(({ promptTemplate }) => promptTemplate === curentPromptTemplate);
+        if (index === -1) {
+            throw new Error(`Prompt template is not in this pipeline`);
+        }
+
+        if (index === this.promptTemplates.length - 1) {
+            return null;
+        }
+
+        return this.promptTemplates[index + 1]!.promptTemplate;
+    }
 }
 
 /**
+ * TODO: Can be Array elegantly typed such as it must have at least one element?
  * TODO: [🧠] There should or should not be a word "GPT" in both createChatThread and completeWithGpt
  */
