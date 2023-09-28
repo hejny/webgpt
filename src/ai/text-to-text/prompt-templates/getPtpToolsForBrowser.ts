8e9ffabd4a658c5ac3b76d328810ff460d45181f
Pavol Hejny <me@pavolhejny.com>
2023-09-28 22:16:55 +0200

    There is no getPtpToolsForServer

--- src/ai/text-to-text/prompt-templates/getPtpToolsForBrowser.ts
+++ src/ai/text-to-text/prompt-templates/getPtpToolsForBrowser.ts
@@ -1,4 +1,6 @@
 import { isRunningInBrowser } from '../../../utils/isRunningInWhatever';
+import { provideClientIdWithoutVerification } from '../../../utils/supabase/provideClientIdWithoutVerification';
+import { RemotePtpExecutionTools } from './lib/src/execution/plugins/remote/RemotePtpExecutionTools';
 import { PtpExecutionTools } from './lib/src/execution/PtpExecutionTools';
 
 /**
@@ -14,7 +16,7 @@ let ptpExecutionTools: PtpExecutionTools;
  * Get PTP execution tools
  *
  * Note: Tools are cached, so it's safe to call this function multiple times
- * Note: This function is available ONLY in browser, use getPtpToolsForServer in node
+ * Note: This function is available ONLY in browser
  *
  * @returns PtpExecutionTools
  */
@@ -24,7 +26,11 @@ export function getPtpToolsForBrowser() {
     }
 
     if (!ptpExecutionTools) {
-        ptpExecutionTools = new PtpRemoteExecutionTools();
+        ptpExecutionTools = new RemotePtpExecutionTools(
+            new URL('http://localhost:4445/' /* <- TODO: !!! Unhardcode */),
+            provideClientIdWithoutVerification(),
+            // TODO: !!! await provideClientId({ isVerifiedEmailRequired: true }),
+        );
     }
 
     return ptpExecutionTools;
