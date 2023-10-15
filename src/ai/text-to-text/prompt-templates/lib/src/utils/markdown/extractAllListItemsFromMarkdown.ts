5f1150716ee125149db9611549a3d27ef7ea19e1
Pavol Hejny <me@pavolhejny.com>
2023-10-15 11:17:43 +0200

    👾 Utility function extractAllListItemsFromMarkdown implemented by AI+TDD

---
+++ src/ai/text-to-text/prompt-templates/lib/src/utils/markdown/extractAllListItemsFromMarkdown.ts
@@ -0,0 +1,32 @@
+/**
+ * Utility function to extract all list items from markdown
+ *
+ * Note: It works with both ul and ol
+ * Note: It omits list items in code blocks
+ * Note: It flattens nested lists
+ * Note: It can not work with html syntax and comments
+ *
+ * @param markdown any valid markdown
+ * @returns
+ */
+export function extractAllListItemsFromMarkdown(markdown: string): string[] {
+    const lines = markdown.split('\n');
+    const listItems: string[] = [];
+
+    let isInCodeBlock = false;
+
+    for (const line of lines) {
+        const trimmedLine = line.trim();
+
+        if (trimmedLine.startsWith('```')) {
+            isInCodeBlock = !isInCodeBlock;
+        }
+
+        if (!isInCodeBlock && (trimmedLine.startsWith('-') || trimmedLine.match(/^\d+\./))) {
+            const listItem = trimmedLine.replace(/^-|\d+\./, '').trim();
+            listItems.push(listItem);
+        }
+    }
+
+    return listItems;
+}
