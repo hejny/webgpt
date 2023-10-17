2b640222cf184f6a24085690ae3adcb72c051685
Pavol Hejny <me@pavolhejny.com>
2023-10-17 23:09:18 +0200

    Split command interface

--- src/ai/text-to-text/prompt-templates/lib/src/types/Command.ts
+++ src/ai/text-to-text/prompt-templates/lib/src/types/Command.ts
@@ -6,24 +6,44 @@ import { ModelRequirements } from './ModelRequirements';
  * Command is one piece of the prompt template which adds some logic to the prompt template or the whole pipeline.
  * It is parsed from the markdown from ul/ol items - one command per one item.
  */
-export type Command =
-    | {
-          type: 'EXECUTE';
-          executionType: ExecutionType;
-      }
-    | {
-          type: 'USE';
-          key: keyof ModelRequirements;
-          value: any /* <- TODO: Infer from used key, can it be done in TypeScript */;
-      }
-    | {
-          type: 'PTP_VERSION';
-          ptpVersion: string_version;
-      }
-    | ParameterCommand;
+export type Command = ExecuteCommand | UseCommand | PtpVersionCommand | ParameterCommand;
 
-// TODO: !!!last Split + annotate each command
+/**
+ * Execute command tells how to execute the section
+ * It can be either prompt template, script or simple template etc.
+ */
+export interface ExecuteCommand {
+    type: 'EXECUTE';
+    executionType: ExecutionType;
+}
 
+/**
+ * Use command tells which model and modelRequirements to use for the prompt template. execution
+ */
+export interface UseCommand {
+    type: 'USE';
+    key: keyof ModelRequirements;
+    value: any /* <- TODO: Infer from used key, can it be done in TypeScript */;
+}
+
+/**
+ * PtpVersion command tells which version is .ptp file using
+ *
+ * - It is used for backward compatibility
+ * - It is defined per whole .ptp file in the header
+ */
+export interface PtpVersionCommand {
+    type: 'PTP_VERSION';
+    ptpVersion: string_version;
+}
+
+/**
+ * Parameter command describes one parameter of the prompt template
+ *
+ * - It can tell if it is input or output parameter
+ * - It can have description
+ * - In description it can have simple formatting BUT not markdown structure or reference to other parameters
+ */
 export interface ParameterCommand {
     type: 'PARAMETER';
     isInputParameter: boolean;
