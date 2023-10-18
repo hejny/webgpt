73b08afb29d7745225eceb7869085c0b9b8f9606
Pavol Hejny <me@pavolhejny.com>
2023-10-18 14:14:55 +0200

    Just one error

--- src/ai/text-to-text/prompt-templates/error.ts
+++ src/ai/text-to-text/prompt-templates/error.ts
@@ -1,5 +1,3 @@
+// Note: Intentional error to prevent build before parsing new prompt templates
 
-Intentional error to prevent build before parsing new prompt templates
-
-
-e[p];265789623947*/*w e25/234-+/525 *
+-
