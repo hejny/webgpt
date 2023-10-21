db8378f7214fc1b51a8a6870cbd738046cb3d4a5
Pavol Hejny <me@pavolhejny.com>
2023-10-21 16:31:34 +0200

    Shells of new execution tools

---
+++ src/ai/text-to-text/prompt-templates/lib/src/execution/plugins/script-execution-tools/javascript/JavascriptEvalExecutionTools.ts
@@ -0,0 +1,11 @@
+import { ScriptExecutionTools } from '../../../ScriptExecutionTools';
+
+export class JavascriptEvalExecutionTools implements ScriptExecutionTools {
+    
+
+}
+
+/**
+ * TODO: !!! Implement
+ * TODO: !!! Annotate
+ */
--- src/ai/text-to-text/prompt-templates/lib/src/execution/plugins/script-execution-tools/javascript/JavascriptExecutionTools.ts
+++ src/ai/text-to-text/prompt-templates/lib/src/execution/plugins/script-execution-tools/javascript/JavascriptExecutionTools.ts
@@ -3,6 +3,6 @@ import { ScriptExecutionTools } from '../../../ScriptExecutionTools';
 export class JavascriptExecutionTools implements ScriptExecutionTools {}
 
 /**
- * TODO: !!! Implement
+ * TODO: !!! Implement via vm2
  * TODO: !!! Annotate
  */
