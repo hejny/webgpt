69cbd683df0ab07c9c250276e9d5021168be7762
Pavol Hejny <me@pavolhejny.com>
2023-10-23 01:58:25 +0200

    Annotate

--- src/ai/text-to-text/prompt-templates/lib/src/execution/ScriptExecutionTools.ts
+++ src/ai/text-to-text/prompt-templates/lib/src/execution/ScriptExecutionTools.ts
@@ -10,12 +10,29 @@ export interface ScriptExecutionTools {
     execute(options: ScriptExecutionToolsExecuteOptions): Promise<string>;
 }
 
-export interface ScriptExecutionToolsExecuteOptions {
-    script: string_script;
-    scriptLanguage: ScriptLanguage;
-    parameters: Record<string_name, string>;
-}
-
 /**
- * !!! Annotate
+ * Input for the script execution
  */
+export interface ScriptExecutionToolsExecuteOptions {
+    /**
+     * Language of the script
+     */
+    scriptLanguage: ScriptLanguage;
+
+    /**
+     * Parameters for the script
+     * Theese parameters are passed to the script as variables
+     * For example: { "name": "John" } => const name = "John";
+     */
+    parameters: Record<string_name, string>;
+
+    /**
+     * The content of the script to execute
+     * - It can be a single statement
+     * - It can be multiple statements separated by semicolon and return
+     * - It can be a function (but you need to call it)
+     * - It can be IIFE (immediately invoked function expression)
+     * - It can use the parameters as variables and functions from global scope
+     */
+    script: string_script;
+}
