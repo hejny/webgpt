e18bd7f7ba868fb78ace9b859286589c39b3b474
Pavol Hejny <me@pavolhejny.com>
2023-10-30 13:10:03 +0100

    Reorganize promptbooks

--- src/ai/prompt-templates/webgptPtpLibrary.ts
+++ src/ai/prompt-templates/webgptPtpLibrary.ts
@@ -1,7 +1,7 @@
-import updateWebsiteContent from '../../../../promptbooks/update-website-content.ptp.md';
-import writeWebsiteContentCs from '../../../../promptbooks/write-website-content.cs.ptp.md';
-import writeWebsiteContent from '../../../../promptbooks/write-website-content.ptp.md';
-import { PromptTemplatePipelineLibrary } from './lib/src/classes/PromptTemplatePipelineLibrary';
+import { PromptTemplatePipelineLibrary } from '@promptbook/core';
+import updateWebsiteContent from '../../../promptbooks/update-website-content.ptp.md';
+import writeWebsiteContentCs from '../../../promptbooks/write-website-content.cs.ptp.md';
+import writeWebsiteContent from '../../../promptbooks/write-website-content.ptp.md';
 
 /**
  * This is The main library of prompt template pipelines for WebGPT app
