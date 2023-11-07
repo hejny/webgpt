d14268599295e3b47901e7cd8c1098f31435450a
Pavol Hejny <me@pavolhejny.com>
2023-11-07 15:23:26 +0100

    Generating images in playground

--- scripts/playground/playground.ts
+++ scripts/playground/playground.ts
@@ -7,7 +7,6 @@ dotenv.config({ path: '.env' });
 import chalk from 'chalk';
 import OpenAI from 'openai';
 import { join } from 'path';
-import spaceTrim from 'spacetrim';
 import { OPENAI_API_KEY } from '../../config';
 
 if (process.cwd() !== join(__dirname, '../..')) {
@@ -35,61 +34,11 @@ async function playground() {
         apiKey: OPENAI_API_KEY,
     });
 
-    const rawResponse = await openai.completions.create({
-        model: 'gpt-3.5-turbo-instruct',
-        max_tokens: 1000,
-        prompt: spaceTrim(`
-            
-            Jako zkušenému copywriterovi a webdesignérovi vám bylo svěřeno vytvoření textu pro novou webovou stránku Nestvořené dětské světy.
-
-            Zadání webu od zákazníka:
-            
-            \`\`\`
-            dítě sedí na zemi
-            \`\`\`
-            
-            ## Pokyny:
-            
-            -   Formátování textu je v Markdownu
-            -   Buďte struční a výstižní
-            -   Použijte klíčová slova, avšak ta mají být přirozeně v textu
-            -   Jedná se o kompletní obsah stránky, tedy nezapomeňte na všechny důležité informace a prvky, co by měla stránka obsahovat
-            -   Použijte nadpisy, odrážky, formátování textu
-            
-            ## Klíčová slova:
-            
-            - Děti
-            - Svět
-            - Kreativita
-            - Hračky
-            - Fantazie
-            - Vytváření
-            - Prostor
-            - Hraní
-            - Inovace
-            - Rozvoj
-            - Představivost
-            - Herní plocha
-            - Design
-            - Hry
-            - Zábava
-            - Interaktivita
-            - Stimulace
-            - Vzdělání
-            - Dětský pokoj
-            - Hra
-            
-            ## Obsah webu:
-            
-            
-            # Nestvořené dětské světy
-            
-            > Domačtěte dětem svět, který nebyl ještě vymyšlený!
-        
-        `),
+    const generatedImage = await openai.images.generate({
+        prompt: 'A dog',
     });
 
-    console.log(rawResponse);
+    console.log(generatedImage);
     //========================================/
 
     console.info(`[ Done 🧸  Playground ]`);
