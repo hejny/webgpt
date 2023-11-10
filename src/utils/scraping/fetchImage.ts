d5d86688ef17380140b6d2ffa1f3558d951dc5ec
Pavol Hejny <me@pavolhejny.com>
2023-11-10 22:33:31 +0100

    fetchImage works in worker

--- src/utils/scraping/fetchImage.ts
+++ src/utils/scraping/fetchImage.ts
@@ -1,25 +1,28 @@
 import { IS_VERIFIED_EMAIL_REQUIRED } from '../../../config';
-import { isRunningInBrowser } from '../isRunningInWhatever';
+import { isRunningInBrowser, isRunningInWebWorker } from '../isRunningInWhatever';
 import { provideClientId } from '../supabase/provideClientId';
 import { string_url_image } from '../typeAliases';
 
 /**
  * Fetch image using proxy
  *
- * Note: This function is available ONLY in browser
+ * Note: This function is available ONLY in browser or web worker
  * @returns image as Blob
  */
 export async function fetchImage(imageUrl: string_url_image): Promise<Blob> {
-    if (!isRunningInBrowser()) {
-        throw new Error(`This function is available ONLY in browser`);
+    if (!isRunningInBrowser() && !isRunningInWebWorker()) {
+        throw new Error(`This function is available ONLY in browser or web worker`);
     }
 
     const response = await fetch(
         // TODO: [🌺][3] Make some wrapper for this apiClient to construct requests + parse them and handle errors
 
-        `/api/scrape/scrape-image?clientId=${/* <- TODO: [⛹️‍♂️] Send clientId through headers */await provideClientId({
-            isVerifiedEmailRequired: IS_VERIFIED_EMAIL_REQUIRED.CREATE /* <- TODO: Is it OK to assume create stage */,
-        })}&url=${encodeURIComponent(imageUrl)}`,
+        `/api/scrape/scrape-image?clientId=${
+            /* <- TODO: [⛹️‍♂️] Send clientId through headers */ await provideClientId({
+                isVerifiedEmailRequired:
+                    IS_VERIFIED_EMAIL_REQUIRED.CREATE /* <- TODO: Is it OK to assume create stage */,
+            })
+        }&url=${encodeURIComponent(imageUrl)}`,
     );
 
     return await response.blob();
