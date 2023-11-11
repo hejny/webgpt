b8d1a85434ece4d0ce78f9265248f046fcd56b85
Pavol Hejny <me@pavolhejny.com>
2023-11-11 09:20:32 +0100

    Scrape image playground

--- src/pages/api/scrape/scrape-image.ts
+++ src/pages/api/scrape/scrape-image.ts
@@ -25,7 +25,11 @@ export default async function scrapeInstagramUserHandler(
     // TODO: [🌺] Log cost for this request and attribute it to the client
     //---------------
 
+
     const url = request.query.url;
+
+    // console.log('request.headers');
+    // const url = request.headers['X-Image-Url'];
 
     if (!isValidUrl(url)) {
         return response.status(400).json(
