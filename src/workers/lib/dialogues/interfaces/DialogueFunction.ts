8ab4fef155118e00105080d14804958135dc3a1f
Pavol Hejny <me@pavolhejny.com>
2023-11-22 23:21:17 +0100

    Change to interface with call signature

--- src/workers/lib/dialogues/interfaces/DialogueFunction.ts
+++ src/workers/lib/dialogues/interfaces/DialogueFunction.ts
@@ -1,12 +1,12 @@
 import { string_name } from '@promptbook/types';
 import { DialogueComponent } from './DialogueComponent';
 
-export type DialogueFunction<TRequest, TResponse> = {
+export interface DialogueFunction<TRequest, TResponse> {
     dialogueTypeName: string_name;
     DialogueComponent: DialogueComponent<TRequest, TResponse>;
-} & ((request: TRequest) => Promise<TResponse>);
+    (request: TRequest): Promise<TResponse>;
+}
 
 /**
- * TODO: Refactor: TODO: !!! Change to interface with call type
  * TODO: !!! Annotate
  */
--- src/workers/lib/interfaces/IWorkerifyableFunction.ts
+++ src/workers/lib/interfaces/IWorkerifyableFunction.ts
@@ -2,12 +2,10 @@ import { Promisable } from 'type-fest';
 import { WebgptTaskProgress } from '../../../components/TaskInProgress/task/WebgptTaskProgress';
 import { TransferableObject } from './TransferableObject';
 
-export type IWorkerifyableFunction<TRequest extends TransferableObject, TResult extends TransferableObject> = (
-    request: TRequest,
-    onProgress: (taskProgress: WebgptTaskProgress) => Promisable<void>,
-) => Promise<TResult>;
+export interface IWorkerifyableFunction<TRequest extends TransferableObject, TResult extends TransferableObject> {
+    (request: TRequest, onProgress: (taskProgress: WebgptTaskProgress) => Promisable<void>): Promise<TResult>;
+}
 
 /**
- * TODO: Refactor: TODO: !!! Change to interface with call type
  * TODO: !!! Annotate
  */
