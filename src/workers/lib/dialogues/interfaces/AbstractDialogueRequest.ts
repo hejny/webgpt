8ad7d9926377bf8ca5e86e00ed9f9a3923b55502
Pavol Hejny <me@pavolhejny.com>
2023-12-02 16:55:44 +0100

    Add priority property to AbstractDialogueRequest interface

--- src/workers/lib/dialogues/interfaces/AbstractDialogueRequest.ts
+++ src/workers/lib/dialogues/interfaces/AbstractDialogueRequest.ts
@@ -3,4 +3,13 @@ import { TransferableObject } from '../../interfaces/TransferableObject';
 /**
  * Abstract request for any dialogue
  */
-export type AbstractDialogueRequest = TransferableObject & {};
+export type AbstractDialogueRequest = TransferableObject & {
+    /**
+     * The priority of the dialogue.
+     * If there are multiple dialogues in the queue, the one with the highest priority will be shown first.
+     * If there will be new dialogues added to the queue, the one with the highest priority will be popped on top in UI.
+     *
+     * @default 0
+     */
+    priority?: number;
+};
