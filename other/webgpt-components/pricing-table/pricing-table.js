865ef10d65c1e8e276aa0017b2f010bb0a551498
Pavol Hejny <me@pavolhejny.com>
2023-12-10 19:09:57 +0100

    WebgptElement

--- other/webgpt-components/pricing-table/pricing-table.js
+++ other/webgpt-components/pricing-table/pricing-table.js
@@ -1,28 +1,46 @@
-class PricingTable extends HTMLElement {
+class WebgptElement extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    assertsToBeDirectChildOf(parentTagName) {
+        // Note: Need to be in timeout to this.parentElement to be defined
+        setTimeout(() => {
+            if (this.parentElement.tagName.toLowerCase() !== parentTagName) {
+                console.error(
+                    `<${this.tagName.toLowerCase()}/> must be a direct child of <${parentTagName}/>, but it is child of <${this.parentElement.tagName.toLowerCase()}/>`,
+                    this,
+                );
+            }
+        }, 1);
+    }
+}
+
+class PricingTable extends WebgptElement {
     constructor() {
         super();
         // TODO: Check that in root
     }
 }
 
-class PricingPlan extends HTMLElement {
+class PricingPlan extends WebgptElement {
     constructor() {
         super();
-        // TODO: Check that direct parent is <pricing-table/>
+        this.assertsToBeDirectChildOf('pricing-table');
     }
 }
 
-class PricingPrice extends HTMLElement {
+class PricingPrice extends WebgptElement {
     constructor() {
         super();
-        // TODO: Check that direct parent is <pricing-plan/>
+        this.assertsToBeDirectChildOf('pricing-plan');
     }
 }
 
-class PricingFeature extends HTMLElement {
+class PricingFeature extends WebgptElement {
     constructor() {
         super();
-        // TODO: Check that direct parent is <pricing-plan/>
+        this.assertsToBeDirectChildOf('pricing-plan');
     }
 }
 
