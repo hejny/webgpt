d568c0e8607d784c3e7d09f7d673523661e28955
Pavol Hejny <me@pavolhejny.com>
2023-12-11 23:11:04 +0100

    Annotate types

--- src/utils/client/verifyEmailCode.types.ts
+++ src/utils/client/verifyEmailCode.types.ts
@@ -1,20 +1,32 @@
 import type { client_id, string_token } from '../typeAliases';
 
+/**
+ * Arguments for $verifyEmailCode function
+ */
 export type VerifyEmailCodeRequest = {
     clientId: client_id;
     email: string_token;
     code: string_token;
 };
 
+/**
+ * Result of $verifyEmailCode function
+ */
 export type VerifyEmailCodeResult =
     | {
+          /**
+           * Indicates the status of client verification
+           */
           status: 'VERIFIED' /*| 'ALREADY_VERIFIED' | 'EXPIRED'*/;
       }
     | {
+          /**
+           * Indicates the failed status of client verification
+           */
           status: 'ERROR';
+
+          /**
+           * Indicates the reason of failure
+           */
           message: string;
       };
-
-/**
- * TODO: !!!last Annotate all
- */
